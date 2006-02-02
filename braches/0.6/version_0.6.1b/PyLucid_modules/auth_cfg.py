49a73207d44cba477a6502d7853f18411c54228f
pylucid <pylucid@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2006-02-02 08:03:53 +0000

    updates

---
+++ braches/0.6/version_0.6.1b/PyLucid_modules/auth_cfg.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#___________________________________________________________________________________________________
+# Meta-Angaben
+
+__author__      = "Jens Diemer"
+__url__         = "http://www.PyLucid.org"
+__description__ = "Login/Logout"
+__long_description__ = """"""
+__essential_buildin__ = True
+
+#___________________________________________________________________________________________________
+# Module-Manager Daten
+
+#~ module_manager_debug = True
+module_manager_debug = False
+
+module_manager_data = {
+    "login" : {
+        "must_login"    : False,
+        "direct_out"    : True,
+        "internal_page_info" : {
+            "description"       : "The Login html-form Page",
+            "template_engine"   : "string formatting",
+            "markup"            : None
+        },
+    },
+    "logout" : {
+        "must_login"    : False,
+        "must_admin"    : False,
+        "direct_out"    : True,
+    },
+    "check_login" : {
+        "must_login"    : False,
+        "direct_out"    : True,
+    },
+}
