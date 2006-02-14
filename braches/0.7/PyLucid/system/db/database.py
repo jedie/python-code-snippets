fd51274cba84e72733db7871112a770c4b0d8da5
pylucid <pylucid@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2006-02-14 07:31:51 +0000

    update comments

--- braches/0.7/PyLucid/system/db/database.py
+++ braches/0.7/PyLucid/system/db/database.py
@@ -4,12 +4,11 @@
 # copyleft: jensdiemer.de (GPL v2 or above)
 
 """
-MySQLdb wrapper
+Stellt die Verbindung zur DB her. Bietet dabei einen DictCursor für alle
+SQL-DBs.
 
-Allgemeine Routinen für eine einfachere SQL Benutzung
-
-Benötigt MySQLdb download unter:
-http://sourceforge.net/projects/mysql-python/
+Links:
+MySQLdb: http://sourceforge.net/projects/mysql-python/
 
 
 Information
@@ -32,29 +31,35 @@ __history__="""
 v0.5
     - Aufteilung: SQL-Wrapper aufgegliedert
 v0.4.1
-    - Work-a-round für fehlendes lastrowid (s. PEP-0249) http://www.python-forum.de/viewtopic.php?p=28819#28819
+    - Work-a-round für fehlendes lastrowid
+        (s. PEP-0249) http://www.python-forum.de/viewtopic.php?p=28819#28819
 v0.4
-    - Verwendet nun einen eigenen Dict-Cursor ( http://pythonwiki.pocoo.org/Dict_Cursor )
+    - Verwendet nun einen eigenen Dict-Cursor
+        ( http://pythonwiki.pocoo.org/Dict_Cursor )
     - Nur mit MySQLdb getestet!
 v0.3
-    - Wenn fetchall verwendet wird, werden in self.last_SQLcommand und self.last_SQL_values die
-        letzten SQL-Aktionen festgehalten. Dies kann gut, für Fehlerausgaben verwendet werden.
+    - Wenn fetchall verwendet wird, werden in self.last_SQLcommand und
+        self.last_SQL_values die letzten SQL-Aktionen festgehalten. Dies kann
+        gut, für Fehlerausgaben verwendet werden.
 v0.2
-    - insert() filtert nun automatisch Keys im Daten-Dict raus, die nicht als Spalte in der Tabelle vorkommen
+    - insert() filtert nun automatisch Keys im Daten-Dict raus, die nicht als
+        Spalte in der Tabelle vorkommen
     - NEU: get_table_field_information() und get_table_fields()
 v0.1.1
     - NEU: decode_sql_results() - Alle Ergebnisse eines SQL-Aufrufs encoden
 v0.1.0
-    - Umbau, nun kann man den table_prefix bei einer Methode auch mit angeben, um auch an Tabellen zu
-        kommen, die anders anfangen als in der config.py festgelegt.
+    - Umbau, nun kann man den table_prefix bei einer Methode auch mit angeben,
+        um auch an Tabellen zu kommen, die anders anfangen als in der
+        config.py festgelegt.
 v0.0.9
     - in update() wird nun auch die where-value SQL-Escaped
 v0.0.8
     - bei select() werden nun auch die select-values mit der SQLdb escaped
     - methode _make_values() gelöscht und einfach durch tuple() erstetzt ;)
 v0.0.7
-    - Statt query_fetch_row() wird der Cursor jetzt mit MySQLdb.cursors.DictCursor erstellt.
-        Somit liefern alle Abfragen ein dict zurück! Das ganze funktioniert auch mit der uralten
+    - Statt query_fetch_row() wird der Cursor jetzt mit
+        MySQLdb.cursors.DictCursor erstellt. Somit liefern alle Abfragen ein
+        dict zurück! Das ganze funktioniert auch mit der uralten
         MySQLdb v0.9.1
     - DEL: query_fetch_row()
     - NEU: fetchall()
