367a5df67e1901a09fbacf8c2ef514617f2304e6
JensDiemer <git@jensdiemer.de>
2006-02-23 16:56:44 +0000

    dbconfig als Klasse zum überschreiben

--- CodeSnippets/PyFileCenter/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-dbconf = {
-    "dbHost"            : 'localhost', # Evtl. muß hier die Domain rein
-    "dbDatabaseName"    : 'DatabaseName',
-    "dbUserName"        : 'UserName',
-    "dbPassword"        : 'Password',
-}
