0a8719bb00541c16773fd0fd491a9a43c88c0863
pylucid <pylucid@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2006-04-03 07:21:22 +0000

    added files

---
+++ CodeSnippets/PyDown/trunk/Win_pasteServerTest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+Diese Konfigurations-Datei ist die eigentliche WSGI-App die vom CGI-Handler
+benutzt wird.
+"""
+
+from PyDown.PyDown import cfg, app
+
+# Überschreiben der Basis-Einstellungen
+#~ cfg["allows_user"]      = ("jedie", "snacker", "nero1976", "idontno")
+cfg["admin_username"]   = "anonymous"
+
+cfg["ext_whitelist"]    = (".mp3",)
+cfg["base_path"]        = "M:/"
+
+cfg["only_https"]       = False
+#~ cfg["only_https"]       = True
+
+cfg["only_auth_users"]  = False
+
+# Zugriffe nur von bestimmten IP's zulassen
+cfg["IP_range"] = ["*.*.*.*"]
+
+cfg["debug"]            = True
+
+
+if __name__ == '__main__':
+    from colubrid import execute
+    execute(app, reload=True)
+
