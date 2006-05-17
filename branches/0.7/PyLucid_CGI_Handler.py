9ddaf501abcf02cceb505511dff3a57d9c854a5f
pylucid <pylucid@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2006-05-17 18:29:45 +0000

    updates

--- branches/0.7/PyLucid_CGI_Handler.py
+++ branches/0.7/PyLucid_CGI_Handler.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+from __future__ import generators
+
 #~ print "Content-type: text/html; charset=utf-8\r\n\r\n<pre>DEBUG:"
 import cgitb;cgitb.enable()
 
+from PyLucid.python_backports.backports import *
 
 import sys
-
 
 from PyLucid import PEP333_WSGI_CGIServer as CGIServer
 
