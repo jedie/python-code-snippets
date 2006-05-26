c6a47eb656bbad160ee6cdbbbaa5a53aec58aa23
pylucid <pylucid@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2006-05-26 21:58:54 +0000

    new module admin

--- branches/0.7/PyLucid/modules/module_admin/module_admin_cfg.py
+++ branches/0.7/PyLucid/modules/module_admin/module_admin_cfg.py
@@ -7,13 +7,11 @@
 __author__              = "Jens Diemer"
 __url__                 = "http://www.PyLucid.org"
 __description__         = "The Module/Plugin Administration"
+__long_description__    = """With the Module/Plugin Administration
+you can install/deinstall modules to the DB and you
+can activate/deactivate this modules in the DB.
+"""
 __essential_buildin__   = True
-
-#~ # Ausnahme: install_template wird in install_PyLucid.py benötigt!
-#~ import os
-#~ f = file(os.path.join("PyLucid/modules","module_admin_administation_menu.html"),"rU")
-#~ install_template = f.read()
-#~ f.close()
 
 #___________________________________________________________________________________________________
 # Module-Manager Daten
@@ -34,7 +32,7 @@ module_manager_data = {
         "menu_description"  : "Administation menu",
         "internal_page_info" : {
             "description"       : "The Module/Plugin Administration page",
-            "template_engine"   : "TAL",
+            "template_engine"   : "jinja",
             "markup"            : None
         },
     },
