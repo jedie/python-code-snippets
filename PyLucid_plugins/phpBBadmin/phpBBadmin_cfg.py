6894bb196bf6fe4c273ceb937df0a1f5b7117fee
JensDiemer <git@jensdiemer.de>
2006-07-06 16:35:36 +0000

    phpBBadmin - delete spam user

---
+++ PyLucid_plugins/phpBBadmin/phpBBadmin_cfg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#___________________________________________________________________________________________________
+# Meta-Angaben
+
+__author__      = "Jens Diemer"
+__url__         = "http://www.PyLucid.org"
+__description__ = __long_description__ = (
+    "phpBBadmin - Delete phpBB spam user accounts"
+)
+
+#___________________________________________________________________________________________________
+# Module-Manager Daten
+
+#~ module_manager_debug = True
+module_manager_debug = False
+
+module_manager_data = {
+    "lucidTag" : {
+        "must_login"    : True,
+        "must_admin"    : False,
+    },
+}
