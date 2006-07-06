1a5c7f5c2e17cb1bb530110e48eab497885016a8
pylucid <pylucid@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2006-07-06 16:35:36 +0000

    phpBBadmin - delete spam user

---
+++ PyLucid_plugins/phpBBadmin/phpBBadmin.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+__author__  = "Jens Diemer (www.jensdiemer.de)"
+__license__ = "GNU General Public License (GPL)"
+__url__     = "http://www.PyLucid.org"
+
+
+
+"""
+PyLucid Plugin - 'phpBBAdmin'
+
+    -löschen von Spamusern
+
+Die Angabe des SQLprefix muß evtl. angepasst werden!
+"""
+
+
+
+__version__="0.1"
+
+__history__= """
+v0.1
+    - init with "delete spamuser" ;)
+"""
+
+
+
+SQLprefix = "phpBB.phpbb_"
+
+
+
+
+from PyLucid.system.BaseModule import PyLucidBaseModule
+
+
+class phpBBadmin(PyLucidBaseModule):
+
+    def lucidTag(self):
+        #~ self.response.debug()
+        self.response.write("<h1>phpBBAdmin</h1>\n")
+
+        if self.request.form.has_key("make"):
+            # Ausgewählte User sollen gelöscht werden
+            self.deleteUsers()
+
+        self.makeTable()
+
+
+    def makeTable(self):
+        """
+        Tabelle zur Auswahl der User aufbauen.
+
+        SELECT * FROM `phpbb_users` WHERE user_posts=0 AND user_lastvisit=0
+        """
+
+        try:
+            spam_user = self.db.select(
+                select_items    = [
+                    "user_id", "user_active", "username", "user_website",
+                    "user_email",
+                ],
+                from_table      = "%susers" % SQLprefix,
+                where           = [
+                    ("user_posts", 0), ("user_lastvisit", 0)
+                ],
+                autoprefix      = False
+            )
+        except Exception, e:
+            self.response.write("<h3>Error:</h3><p>%s</p>\n" % e)
+            self.response.write(
+                "<p>Is the SQLprefix '%s' correct?</p>\n" % SQLprefix
+            )
+            return
+
+        if spam_user==[]:
+            self.response.write("No Spamuser found in DB ;)\n")
+            return
+
+        # HTML-checkbox in Datensatz einfügen
+        for line in spam_user:
+            line["delete User?"] = (
+                '<input type="checkbox" name="delete_id"'
+                ' value="%s" />'
+            ) % line["user_id"]
+
+        self.response.write(
+            '<p>Here a list of every spam user.'
+            ' (user_posts=0 and user_lastvisit=0)</p>\n'
+        )
+
+        self.response.write(
+            '<form method="post" action="%s">\n' % self.URLs.currentAction()
+        )
+        # Tabelle erzeugen
+        self.tools.writeDictListTable(
+            spam_user, self.response, primaryKey="username"
+        )
+        self.response.write(
+            '<p>Disallow username: '
+            '<input type="checkbox" name="disallow" value="%s"'
+            ' checked="checked" /></p>\n'
+            '<input type="submit" value="make" name="make" />\n'
+            '</form>\n'
+        )
+
+    def deleteUsers(self):
+        if not self.request.form.has_key("delete_id"):
+            self.page_msg("No User selected!")
+            return
+
+        IDs = self.request.form.getlist("delete_id")
+        for id in IDs:
+            self.deleteUser(id)
+
+    def deleteUser(self, id):
+        """
+        phpBB Userprofil löschen
+        """
+        user_name = self.db.select(
+            select_items    = ["username"],
+            from_table      = "%susers" % SQLprefix,
+            where           = ("user_id", id),
+            autoprefix      = False
+        )
+        user_name = user_name[0]["username"]
+        if self.request.form.has_key("disallow"):
+            self.disallowUser(user_name)
+
+        self.page_msg(
+            "Delete user '%s' (id %s)." % (user_name, id)
+        )
+
+        self.db.delete(
+            table       = "%susers" % SQLprefix,
+            where       = ("user_id", id),
+            limit       = 1,
+            autoprefix  = False
+        )
+
+    def disallowUser(self, user_name):
+        """
+        Den Usernamen verbieten
+        """
+
+        self.page_msg(
+            "Insert '%s' in disallowed usernames." % (user_name)
+        )
+
+        self.db.insert(
+            table = "%sdisallow" % SQLprefix,
+            data  = { "disallow_username" : user_name },
+            autoprefix  = False
+        )
+
+
+
+
