5900dcec976ba0613c6dc12e2291c610d4648568
JensDiemer <git@jensdiemer.de>
2006-07-10 15:55:59 +0000

    cleanup handler files

--- branches/0.7/0 PyLucid_app Loop.sh
+++ branches/0.7/0 PyLucid_app Loop.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+# Startet einen lokalen Test-Server.
+
 while :
 do
     echo 'Starte Webserver...'
+
+    # für Python 2.4:
     python PyLucid_app.py
+
+    # für Python <2.4:
+    # python PyLucid_app_withBackports.py
+
     echo ''
-    echo 'Enter drücken!'
+    echo 'restart des Servers mit ENTER...'
     read
 done
 
-echo 'Enter zum Beenden.'
+echo 'ENTER zum Beenden.'
 read
