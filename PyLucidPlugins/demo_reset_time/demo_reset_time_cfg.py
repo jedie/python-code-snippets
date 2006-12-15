2150af59bace96a7dd0a5a3e7cfe79c20f9d09e5
JensDiemer <git@jensdiemer.de>
2006-12-15 19:32:15 +0000

    +demo reset time

---
+++ PyLucidPlugins/demo_reset_time/demo_reset_time_cfg.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#___________________________________________________________________________________________________
+# Meta-Angaben
+
+__author__      = "PyLucid Team"
+__url__         = "http://www.PyLucid.org"
+__description__ = __long_description__ = (
+    "Display the next DEMO-reset time"
+)
+
+#___________________________________________________________________________________________________
+# Module-Manager Daten
+
+module_manager_data = {
+    "lucidTag"  : {
+        "must_login"    : False,
+        "must_admin"    : False,
+    },
+}
