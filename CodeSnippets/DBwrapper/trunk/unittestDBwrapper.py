d21a36e3e7e5ca9b1377e25d30c96ad57bdeaf1d
jensdiemer <jensdiemer@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2007-01-04 15:42:34 +0000

    unittestDBwrapper.py small changes, but not complete!

--- CodeSnippets/DBwrapper/trunk/unittestDBwrapper.py
+++ CodeSnippets/DBwrapper/trunk/unittestDBwrapper.py
@@ -14,7 +14,7 @@ from DBwrapper import SQL_wrapper
 
 # Note: You must manually edit the Host-Adress of the MySQL-Server!
 MySQLserver = "localhost"
-MySQLserver = "192.168.6.2"
+#~ MySQLserver = "192.168.6.2"
 
 
 #~ debug = debug
@@ -28,30 +28,6 @@ class testDBwrapper(unittest.TestCase):
         self.db = SQL_wrapper(sys.stdout)
         self.db.tableprefix="unittest_"
 
-    def test_sqlite(self):
-        print "\n\nSQLite test"
-
-        self.db.connect_sqlite(":memory:")
-        if debug:
-            print "self.dbtyp.......:", self.db.self.dbtyp
-            print "tableprefix.:", self.db.tableprefix
-            print "paramstyle..:", self.db.paramstyle
-            print "placeholder.:", self.db.placeholder
-
-        # Create Table
-        createTableStatement = (
-            "CREATE TABLE $$TestTable ("
-            "id INTEGER PRIMARY KEY,"
-            "data1 VARCHAR(50) NOT NULL,"
-            "data2 VARCHAR(50) NOT NULL"
-            ");"
-        )
-        try:
-            self.check(createTableStatement)
-        finally:
-            self.cleanup()
-
-    def test_mysql(self):
         print "\n\nMySQL test"
 
         self.db.connect_mysqldb(
@@ -66,52 +42,59 @@ class testDBwrapper(unittest.TestCase):
             print "paramstyle..:", self.db.paramstyle
             print "placeholder.:", self.db.placeholder
 
+
+    def tearDown(self):
+        try:
+            print "Delete the temporary test Table."
+            self.db.cursor.execute("DROP TABLE $$test_table")
+            self.assertEqual(self.db.get_tables(), [])
+        finally:
+            print "Close SQL-connection..."
+            self.db.close()
+
+
+    def _create_table(self):
         # Create Table
         createTableStatement = (
-            "CREATE TABLE $$TestTable ("
+            "CREATE TABLE $$test_table ("
             "id INT( 11 ) NOT NULL AUTO_INCREMENT,"
             "data1 VARCHAR( 50 ) NOT NULL,"
             "data2 VARCHAR( 50 ) NOT NULL,"
             "PRIMARY KEY ( id )"
             ") TYPE = InnoDB COMMENT = '%s Temporary test table';"
         ) % __file__
-        try:
-            self.check(createTableStatement)
-        finally:
-            self.cleanup()
-
-    def cleanup(self):
-        try:
-            print "Delete the temporary test Table."
-            self.db.cursor.execute("DROP TABLE $$TestTable")
-            self.assertEqual(self.db.get_tables(), [])
-        finally:
-            print "Close SQL-connection..."
-            self.db.close()
-
-    def check(self, createTableStatement):
-        print "- "*40
-
-        print "get_tables()"
-        self.assertEqual(self.db.get_tables(), [])
-
         print "Creat a temporary test table - execute SQL-command directly"
         self.db.cursor.execute(createTableStatement)
         self.db.commit()
+        print "- "*40
+
+
+    def test_get_tables1(self):
+        print "get_tables()"
+        self.assertEqual(self.db.get_tables(), [])
+
+
+    def test_get_tables2(self):
+        self._create_table()
         tables = self.db.get_tables()
         if debug: print tables
-        self.assertEqual(tables, [u'unittest_TestTable'])
+        self.assertEqual(tables, [u'unittest_test_table'])
 
 
+    def test_table_fields(self):
+        self._create_table()
         print "get_table_fields()"
-        table_fields = self.db.get_table_fields("TestTable")
+        table_fields = self.db.get_table_fields("test_table")
         if debug: print table_fields
         self.assertEqual(table_fields, ['id', 'data1', 'data2'])
 
 
+    def test_mysql(self):
+        self._create_table()
+
         print "INSERT line 1"
         self.db.insert(
-            table = "TestTable",
+            table = "test_table",
             data  = { "data1" : "Value A 1", "data2" : "Value A 2" },
             debug = debug
         )
@@ -122,7 +105,7 @@ class testDBwrapper(unittest.TestCase):
 
         print "INSERT line 2"
         self.db.insert(
-            table = "TestTable",
+            table = "test_table",
             data  = { "data1" : "Value B 1", "data2" : "Value B 2" },
             debug = debug
         )
@@ -134,7 +117,7 @@ class testDBwrapper(unittest.TestCase):
         print "SELECT"
         result = self.db.select(
             select_items    = ["id","data1","data2"],
-            from_table      = "TestTable",
+            from_table      = "test_table",
             where           = ("data1","Value B 1"),
             debug           = debug
         )
@@ -148,7 +131,7 @@ class testDBwrapper(unittest.TestCase):
         result = self.db.get_tableDict(
             select_items = ["data1","data2"],
             index_key = "id",
-            table_name = "TestTable",
+            table_name = "test_table",
         )
         self.assertEqual(
             result,
@@ -160,7 +143,7 @@ class testDBwrapper(unittest.TestCase):
 
         print "DELETE line 1"
         self.db.delete(
-            table = "TestTable",
+            table = "test_table",
             where = ("id",1),
             limit = 1,
             debug = debug
@@ -168,7 +151,7 @@ class testDBwrapper(unittest.TestCase):
         print "SELECT"
         result = self.db.select(
             select_items    = ["id","data1","data2"],
-            from_table      = "TestTable",
+            from_table      = "test_table",
             #~ where           = ("data1","Value B 1"),
             debug           = debug
         )
@@ -179,11 +162,11 @@ class testDBwrapper(unittest.TestCase):
         )
 
         if debug:
-            self.db.dump_table("TestTable")
+            self.db.dump_table("test_table")
 
         print "UPDATE line 2"
         self.db.update(
-            table   = "TestTable",
+            table   = "test_table",
             data    = {"data1" : "NewValue1!"},
             where   = ("id",2),
             debug = debug
@@ -194,7 +177,7 @@ class testDBwrapper(unittest.TestCase):
         print "SELECT *"
         result = self.db.select(
             select_items    = ["id","data1","data2"],
-            from_table      = "TestTable",
+            from_table      = "test_table",
         )
         if debug: self.db.dump_select_result(result)
         self.assertEqual(
@@ -202,31 +185,31 @@ class testDBwrapper(unittest.TestCase):
             [{'data1': u'NewValue1!', 'id': 2, 'data2': u'Value B 2'}]
         )
 
-
+    def test_transaction(self):
         print "Test Transaction:"
-        self.db.insert("TestTable", {"data1": "tranceact1", "data2": "1"})
+        self.db.insert("test_table", {"data1": "tranceact1", "data2": "1"})
         id1 = self.db.cursor.lastrowid
-        self.db.insert("TestTable", {"data1": "tranceact2", "data2": "2"})
+        self.db.insert("test_table", {"data1": "tranceact2", "data2": "2"})
         id2 = self.db.cursor.lastrowid
         self.db.delete(
-            table = "TestTable",
+            table = "test_table",
             where = ("id",id1),
         )
         self.db.update(
-            table   = "TestTable",
+            table   = "test_table",
             data    = {"data1": "tranceact3"},
             where   = ("id",id2)
         )
         result = self.db.select(
             select_items    = "*",
-            from_table      = "TestTable",
+            from_table      = "test_table",
             order           = ("id","ASC"),
         )
         if debug: self.db.dump_select_result(result)
         self.db.rollback() # Alle letzten Befehle rückgängig machen
         result = self.db.select(
             select_items    = "*",
-            from_table      = "TestTable",
+            from_table      = "test_table",
             order           = ("id","ASC"),
         )
         if debug: self.db.dump_select_result(result)
