8ba701a8202bad2ffbf92f1217b87bca89f4b826
JensDiemer <git@jensdiemer.de>
2007-02-20 11:04:05 +0000

    django pagestats.py middleware

--- branches/0.8(django)/PyLucid/settings.py
+++ branches/0.8(django)/PyLucid/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
+    'PyLucid.middlewares.pagestats.PageStatsMiddleware',
 )
 
 ROOT_URLCONF = 'PyLucid.urls'
