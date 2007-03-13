3edac01673751b9d79e83840f333285f96100c72
JensDiemer <git@jensdiemer.de>
2007-03-13 17:33:21 +0000

    _install updates

--- branches/0.8(django)/PyLucid/install/install.py
+++ branches/0.8(django)/PyLucid/install/install.py
@@ -8,73 +8,136 @@ http://code.djangoproject.com/wiki/CookBookScriptsMiniFlush
 
 import sys, os, StringIO
 
+from PyLucid.install.tools import render
+from PyLucid import settings
 from PyLucid.utils import check_pass
+from PyLucid.tools.OutBuffer import Redirector
 from PyLucid.settings import TABLE_PREFIX
 from PyLucid.system.response import PyLucidResponse
 
-from django.http import HttpResponse
+from django import newforms as forms
 
 #______________________________________________________________________________
 
+syncdb_template = """
+{% extends "PyLucid/install/base.html" %}
+{% block content %}
+<h1>syncdb</h1>
+<pre>{{ output|escape }}</pre>
+{% endblock %}
+"""
 def syncdb(request, install_pass):
     """
     1. install Db tables (syncdb)
     """
     check_pass(install_pass)
 
-    response = PyLucidResponse(request, mimetype="text/plain")
-    old_stdout = sys.stdout
-    old_stderr = sys.stderr
-    sys.stdout = response
-    sys.stderr = response
-    print "django syncdb..."
+    out = Redirector(sys.stderr)
+    output = ["django syncdb..."]
     try:
         from django.core import management
         management.syncdb(verbosity=2, interactive=False)
     except Exception, e:
         sys.stderr.write("Error: %s\n" % e)
-
-    print "done."
-
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-
-    return response
+    
+    output.append(out.get())
+    output.append("done.")
+    
+    context = {"output": "".join(output)}
+    return render(context, syncdb_template)
 
 #______________________________________________________________________________
+class InitDBForm(forms.Form):
+    """
+    django newforms
+    """
+    from glob import glob
+    fnmatch = os.path.join(settings.INSTALL_DATA_DIR, "initial_data.*")
+    fixture_filenames = glob(fnmatch)
+        
+    fixture_file = forms.ChoiceField(
+        choices=[(i,i) for i in fixture_filenames],
+        widget=forms.RadioSelect,
+        help_text='Please select a fixture file.'
+    )
+
+dump_template = """
+{% extends "PyLucid/install/base.html" %}
+{% block content %}
+<h1>init DB</h1>
+
+{% if output %}
+    <fieldset><legend>{{ file_name|escape }}</legend>
+        <pre>{{ output|escape }}</pre>
+    </fieldset>
+{% else %}
+    <form method="post">
+        {{ FormData }}
+        <input value="execute" name="execute" type="submit">
+    </form>
+{% endif %}
+{% endblock %}
+"""
 format = "xml"
-#format = "json"
-#format = "python"
-#format = "pyyaml"
 def init_db(request, install_pass):
     """
     2. init DB data
     """
     check_pass(install_pass)
-    response = HttpResponse(mimetype="text/plain")
+   
+#    fixture_filename = os.path.join(
+#        settings.INSTALL_DATA_DIR, "initial_data.%s" % format
+#    )
     
-    fixture_filename = "PyLucid/fixtures/initial_data.%s" % format
-    f = file(fixture_filename, "r")
-    fixture = f.read()
-    f.close()
-    
-    from django.core import serializers
-    #print serializers.get_serializer_formats()
-    #serializer = serializers.get_serializer("python")
-    objects = serializers.deserialize(format, fixture)
-    
-    count = 0
-    for object in objects:
-        try:
-            object.save()
-        except Exception, e:
-            response.write("Error: %s\n" % e)
-        else:
-            count += 1
+    if "fixture_file" in request.POST:
+        # Form has been sended
+        init_values = request.POST.copy()
+    else:
+        # Requested the first time -> insert a init codeblock
+        init_values = None
         
-    response.write("%s objects restored\n" % count)
+    init_db_form = InitDBForm(init_values)
+    init_db_html = init_db_form.as_p()
     
-    return response
+    context = {
+        "FormData": init_db_html
+    }
+    output = []
+    if "fixture_file" in request.POST and init_db_form.is_valid():
+        fixture_file = init_db_form.clean_data["fixture_file"]
+        context["file_name"] = fixture_file
+        
+        output.append("Read fixture file '%s'..." % fixture_file)
+        try:
+            f = file(fixture_file, "rb")
+    #        import codecs
+    #        f = codecs.open(fixture_filename, "r", "utf-8")
+            fixture = f.read()
+    #        fixture = unicode(fixture, "utf8")
+    #        #fixture = unicode(fixture, errors="replace")
+            f.close()
+        except Exception, e:
+            output.append("Error: %s" % e)
+        else:
+            output.append("OK\n")
+            
+            from django.core import serializers
+    
+            objects = serializers.deserialize(format, fixture)
+            
+            count = 0
+            for object in objects:
+                try:
+                    object.save()
+                except Exception, e:
+                    output.append("Error: %s\n" % e)
+                else:
+                    count += 1
+                
+            output.append("%s objects restored\n" % count)
+    
+    context["output"] = "".join(output)
+    return render(context, dump_template)
 
 #______________________________________________________________________________
 
