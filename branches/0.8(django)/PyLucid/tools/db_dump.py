ed60cdeac4474553a8bdc7b376b336d02ca7e84e
JensDiemer <git@jensdiemer.de>
2007-05-16 15:16:42 +0000

    update db_dump.py v1.6 by limodou from http://www.djangosnippets.org/snippets/14/

--- branches/0.8(django)/PyLucid/tools/db_dump.py
+++ branches/0.8(django)/PyLucid/tools/db_dump.py
@@ -1,3 +1,5 @@
+# http://www.djangosnippets.org/snippets/14/
+#
 # Author: limodou (limodou@gmail.com)
 #
 # This tool is used for dump and reload data from and into database
@@ -5,10 +7,10 @@
 #
 #     python db_dump.py -h
 #
-# For now, it only support .py format, so the output result will 
+# For now, it only support .py format, so the output result will
 # be saved as python source code, and you can import it.
 #
-# Version 1.4 2007-01-20
+# Version 1.6 2007-04-09
 #
 # Update 1.0 2007-01-18
 #
@@ -26,10 +28,16 @@
 # Update 1.4 2007-01-21
 #    * support mysql
 #
+# Update 1.5 2007-02-08
+#    * If the filename is not exists, then skip it
+#
+# Update 1.6 2007-04-09
+#    * Add float support
 
 import os, sys
 from optparse import OptionParser
 import datetime
+import decimal
 
 def _get_table_order(app_labels):
     from django.db.models import get_app, get_apps, get_models
@@ -37,16 +45,16 @@ def _get_table_order(app_labels):
 
     if not app_labels:
         app_list = get_apps()
-    else: 
-        app_list = [get_app(app_label) for app_label in app_labels] 
-    
+    else:
+        app_list = [get_app(app_label) for app_label in app_labels]
+
     models = {}
-    for app in app_list: 
-        for model in get_models(app): 
+    for app in app_list:
+        for model in get_models(app):
             models[model._meta.db_table] = model
-    s = []      
-    rules = [] 
-    
+    s = []
+    rules = []
+
     def order(s, rule):
         a, b = rule
         try:
@@ -66,7 +74,7 @@ def _get_table_order(app_labels):
                 s.append(b)
             except:
                 s.append(b)
-            
+
     for i, table in enumerate(models.keys()[:]):
         for field in models[table]._meta.fields:
             if isinstance(field, (ForeignKey, OneToOneField)):
@@ -80,7 +88,7 @@ def _get_table_order(app_labels):
     for k, v in models.items():
         if s.count(k) == 0:
             n.append(k)
-            
+
     return [models[k] for k in s+n]
 
 def _find_key(d, key):
@@ -96,11 +104,11 @@ def _find_key(d, key):
 
 def loaddb(app_labels, format, options):
     from django.db import connection, transaction, backend
-    
 
-    if options.verbose: 
-        print "Begin to load data for %s format...\n" % format 
-    
+
+    if options.verbose:
+        print "Begin to load data for %s format...\n" % format
+
     models = _get_table_order(app_labels)
 
     cursor = connection.cursor()
@@ -114,28 +122,28 @@ def loaddb(app_labels, format, options):
             cursor.execute('DELETE FROM %s WHERE 1=1;' % backend.quote_name(model._meta.db_table))
             for table, fields in get_model_many2many_stru(model):
                 cursor.execute('DELETE FROM %s WHERE 1=1;' % backend.quote_name(table))
-    
+
     success = True
-    for model in models: 
+    for model in models:
         try:
             load_model(cursor, model, format, options)
             for table, fields in get_model_many2many_stru(model):
                 load_model(cursor, (table, fields), format, options)
-        except Exception, e: 
+        except Exception, e:
             success = False
             errornum += 1
-        
+
     if success:
-        transaction.commit_unless_managed() 
+        transaction.commit_unless_managed()
     else:
         transaction.rollback_unless_managed()
-    
+
     if errornum:
-        print "There are %d errors found!" % errornum
+        print "There are %d errors found! The database has been rollbacked!" % errornum
     else:
         print "Successful!"
-    
-def load_model(cursor, model, format, options): 
+
+def load_model(cursor, model, format, options):
     from django.db import backend
 
     datadir, verbose, stdout = options.datadir, options.verbose, options.stdout
@@ -148,9 +156,13 @@ def load_model(cursor, model, format, options):
         opts = model._meta
         filename = os.path.join(datadir, opts.db_table + '.%s' % format)
         fields, default = get_model_stru(model)
-        
+
     if verbose:
         print '..Dealing %s for %s format...\n' % (filename, format)
+    if not os.path.exists(filename):
+        if verbose:
+            print '..%s does not exists, so Skip it..\n' % filename
+        return
     try:
         objs = {}
         if format == 'py':
@@ -169,15 +181,15 @@ def load_model(cursor, model, format, options):
                     objs['records'] = f
                     break
         else:
-#            f = file(filename, 'rb') 
+#            f = file(filename, 'rb')
 #            objs = f.read()
 #            records = objs['records']
 #            f.close()
             raise 'Not support this format %s' % format
-            
+
         fs = objs['fields']
         table = objs['table']
-        
+
         default.update(objs.get('default', {}))
         count = 0
         for row in objs["records"]:
@@ -200,9 +212,9 @@ def load_model(cursor, model, format, options):
                             try:
                                 v = d[value]
                             except KeyError:
-                                sys.stderr.write("Referenced field [%s] does not exist\n" % value) 
+                                sys.stderr.write("Referenced field [%s] does not exist\n" % value)
                                 raise
-                                
+
                         elif kind == 'date':
                             if not value or value == 'now':
                                 v = datetime.date.today().strftime('%Y-%m-%d')
@@ -218,7 +230,7 @@ def load_model(cursor, model, format, options):
                 if v is not None:
                     sql_fields.append(fd)
                     sql_values.append(v)
-            e_sql = sql % (backend.quote_name(table), 
+            e_sql = sql % (backend.quote_name(table),
                 ','.join(map(backend.quote_name, sql_fields)), ','.join(['%s'] * len(sql_fields)))
             if stdout:
                 print e_sql, sql_values, '\n'
@@ -233,14 +245,14 @@ def load_model(cursor, model, format, options):
             print '(Total %d records)\n' % count
     except Exception, e:
         import traceback
-        traceback.print_exc() 
-        sys.stderr.write("Problem loading %s format '%s' : %s\n" %  
-                 (format, filename, str(e))) 
+        traceback.print_exc()
+        sys.stderr.write("Problem loading %s format '%s' : %s\n" %
+                 (format, filename, str(e)))
         raise
 
 def get_model_stru(model):
     from django.db.models.fields import DateField, DateTimeField, TimeField
-    
+
     fields = []
     default = {}
     for f in model._meta.fields:
@@ -256,7 +268,7 @@ def get_model_stru(model):
 
 def get_model_many2many_stru(model):
     from django.db.models import GenericRel
-    
+
     opts = model._meta
     for f in opts.many_to_many:
         fields = []
@@ -265,36 +277,36 @@ def get_model_many2many_stru(model):
             fields.append(f.m2m_column_name())
             fields.append(f.m2m_reverse_name())
             yield f.m2m_db_table(), fields
-    
-    
-def dumpdb(app_labels, format, options): 
+
+
+def dumpdb(app_labels, format, options):
     from django.db.models import get_app, get_apps, get_models
 
     datadir, verbose, stdout = options.datadir, options.verbose, options.stdout
-    
-    if verbose: 
-        print "Begin to dump data for %s format...\n" % format 
-  
-    if len(app_labels) == 0: 
-        app_list = get_apps() 
-    else: 
-        app_list = [get_app(app_label) for app_label in app_labels] 
-  
+
+    if verbose:
+        print "Begin to dump data for %s format...\n" % format
+
+    if len(app_labels) == 0:
+        app_list = get_apps()
+    else:
+        app_list = [get_app(app_label) for app_label in app_labels]
+
     if not os.path.exists(datadir):
         os.makedirs(datadir)
-      
+
     errornum = 0
-    for app in app_list: 
-        for model in get_models(app): 
+    for app in app_list:
+        for model in get_models(app):
             try:
                 write_result(dump_model(model), format, options)
 
                 for result in dump_many2many(model):
                     write_result(result, format, options)
-            except Exception, e: 
+            except Exception, e:
                 import traceback
                 traceback.print_exc()
-                sys.stderr.write("Unable to dump database: %s\n" % e) 
+                sys.stderr.write("Unable to dump database: %s\n" % e)
                 errornum += 1
 
     if errornum:
@@ -308,7 +320,7 @@ def dump_model(model):
     opts = model._meta
     cursor = connection.cursor()
     fields, default = get_model_stru(model)
-    cursor.execute('select %s from %s' % 
+    cursor.execute('select %s from %s' %
         (','.join(map(backend.quote_name, fields)), backend.quote_name(opts.db_table)))
     return call_cursor(opts.db_table, fields, cursor)
 
@@ -330,15 +342,18 @@ def _pre_data(row):
             row[i] = row[i].strftime('%Y-%m-%d %H:%M:%S') # + '.' + str(row[i].microsecond).rstrip('0')
         elif isinstance(fd, datetime.date):
             row[i] = row[i].strftime('%Y-%m-%d')
+        elif isinstance(fd, decimal.Decimal):
+            row[i] = row[i].__float__()
+
     return row
 
 def dump_many2many(model):
     from django.db import connection, backend
-    
+
     cursor = connection.cursor()
 
     for table, fields in get_model_many2many_stru(model):
-        cursor.execute('select %s from %s' % 
+        cursor.execute('select %s from %s' %
             (','.join(map(backend.quote_name, fields)), backend.quote_name(table)))
         yield call_cursor(table, fields, cursor)
 
@@ -354,6 +369,8 @@ def write_result(result, format, options):
         f = sys.stdout
     print >>f, 'table = %r' % table
     print >>f, 'fields = %r' % fields
+    print >>f, '#default item format: "fieldname":("type", "value")'
+    print >>f, 'default = {}'
     print >>f, 'records = ['
     i = 0
     for t in result:
@@ -364,7 +381,7 @@ def write_result(result, format, options):
         print '(Total %d records)\n' % i
     if not options.stdout:
         f.close()
-    
+
 def get_usage():
     usage = """
   %prog [options] action [applist]:
@@ -389,11 +406,11 @@ def execute_from_command_line(argv=None):
     parser.add_option('-r', '--remain', help='Remain the records of the tables, default will delete all the records. Only used for loading.', action='store_true')
 
     options, args = parser.parse_args(argv[1:])
-    
+
     if len(args) == 0:
         parser.print_help()
         sys.exit(0)
-        
+
     action = args[0]
     apps = args[1:]
     if options.settings:
@@ -406,14 +423,12 @@ def execute_from_command_line(argv=None):
             print "You don't appear to have a settings file in this directory!"
             print "Please run this from inside a project directory"
             sys.exit()
-            
+
         setup_environ(settings)
-        
+
     if action == 'dump':
         dumpdb(apps, 'py', options)
     elif action == 'load':
-        print apps
-        print options
         loaddb(apps, 'py', options)
     else:
         parser.print_help()
