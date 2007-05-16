d6a07c20863e0e52eb3dc9e75633572bd7d9f5e0
JensDiemer <JensDiemer@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2007-05-16 16:31:02 +0000

    update install data

--- branches/0.8(django)/PyLucid/db_dump_datadir/auth_user.py
+++ branches/0.8(django)/PyLucid/db_dump_datadir/auth_user.py
@@ -1,5 +1,7 @@
 table = 'auth_user'
 fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'is_staff', 'is_active', 'is_superuser', 'last_login', 'date_joined']
+#default item format: "fieldname":("type", "value")
+default = {}
 records = [
-[1, 'test', '', '', 'test@invalid', 'sha1$c3459$929ae186bd4cec788e33c877dcb3f19faad96f1c', True, True, True, '2007-03-16 03:41:00', '2007-03-02 08:58:45']
+[1L, 'test', '', '', 'test@invalid', 'sha1$e35ae$fde12d36066669e4d92cb2175e131500bb0bd218', 1, 1, 1, '2007-05-16 07:12:04', '2007-02-19 12:56:10']
 ]
