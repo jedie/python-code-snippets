6ecb971f0c0f52a21bccc8124013eb8149c6c13a
JensDiemer <JensDiemer@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2007-05-16 20:15:44 +0000

    parent is None if its a root page

--- branches/0.8(django)/PyLucid/buildin_plugins/main_menu/main_menu.py
+++ branches/0.8(django)/PyLucid/buildin_plugins/main_menu/main_menu.py
@@ -48,9 +48,10 @@ class main_menu(PyLucidBaseModule):
         menu_data = Page.objects.values(
             "id", "parent", "name", "title", "shortcut"
         ).order_by("position")
-        #self.page_msg(values)
+#        self.page_msg(menu_data)
         menu_data = TreeGenerator().generate(menu_data)
 #        self.page_msg(menu_data)
+
 
 #        # "Startpunkt" für die Menügenerierung feststellen
 #        parentID = self.request.current_page.parent
@@ -130,115 +131,117 @@ class main_menu(PyLucidBaseModule):
         return "\n".join(result)
 
 
-    def where_filter(self, where_rules):
-        """
-        Erweitert das SQL-where Statement um das Rechtemanagement zu
-        berücksichtigen.
-        Selbe Funktion ist auch bei sub_menu vorhanden
-        """
-        where_rules.append(("showlinks",1))
-        #~ if not self.session.get("isadmin", False):
-            #~ where_rules.append(("permitViewPublic",1))
-
-        return where_rules
-
-    def check_submenu(self, parentID, internal=False):
-        """
-        Damit sich das evtl. vorhandene Untermenüpunkt "aufklappt" wird
-        nachgesehen ob ein Menüpunkt als ParentID die aktuelle SeitenID hat.
-        """
-        where_filter = self.where_filter( [("parent",self.current_page_id)] )
-        # Gibt es Untermenupunkte?
-        result = self.db.select(
-                select_items    = ["parent"],
-                from_table      = "page",
-                where           = where_filter,
-                limit           = (0,1)
-            )
-
-        if not result:
-            # Es gibt keine höhere Ebene (keine Untermenupunkte)
-            return parentID
-        else:
-            # Als startpunkt wird die ParentID eines Untermenupunktes übergeben
-            return result[0]["parent"]
-
-    def create_menudata(self, parentID):
-        """
-        Hohlt die relevanten Menüdaten aus der DB in einer Rekursiven-Schleife
-        """
-        # Alle Daten der aktuellen Ebene hohlen
-        parents = self.db.select(
-                select_items    = ["id","name","shortcut","title","parent"],
-                from_table      = "page",
-                where           = self.where_filter( [("parent",parentID)] ),
-                order           = ("position","ASC")
-            )
-        self.menudata.append( parents )
-
-        # Hohlt die parentID, um zur nächte Ebene zurück gehen zu können
-        parent = self.db.select(
-                select_items    = ["parent"],
-                from_table      = "page",
-                where           = [ ("id",parentID) ]
-            )
-        if parent:
-            # Unterste Ebene noch nicht erreicht -> rekursiver Aufruf
-            self.create_menudata(parent[0]["parent"])
-
-    def make_menu(self, menulevel=0, parentname=""):
-        """
-        Erstellt das Menü aus self.menudata in einer Rekursiven-Schleife
-        """
-        result = []
-
-        # Daten der Aktuellen Menüebene
-        leveldata = self.menudata[menulevel]
-
-        if len(self.menudata) > (menulevel+1):
-            # Es gibt noch eine höhere Menu-Ebene
-            try:
-                higher_level_parent = self.menudata[menulevel+1][0]["parent"]
-            except IndexError:
-                # Aber nicht, wenn die aktuelle Seite "versteckt" ist
-                higher_level_parent = False
-        else:
-            # Es gibt keine höhere Ebene
-            higher_level_parent = False
-
-        for menuitem in leveldata:
-            name = menuitem["name"]
-            title = menuitem["title"]
-            if title == None:
-                title = name
-
-            link = "%s/%s" % (parentname, menuitem["shortcut"])
-            linkURL = link#self.URLs.pageLink(link)
-
-            htmlLink = {
-                "level"     : menulevel,
-                "href"      : linkURL,
-                "name"      : name,
-                "title"     : title
-            }
-
-            if menuitem["id"] == self.current_page_id:
-                # Der aktuelle Menüpunkt ist der "angeklickte"
-                htmlLink["is_current_page"] = True
-
-            if higher_level_parent != False:
-                # Generell gibt es noch eine höhere Ebene
-
-                if menuitem["id"] == higher_level_parent:
-                    # Es wurde der Menüpunkt erreicht, der das Untermenü
-                    # "enthält", deswegen kommt ab hier erstmal das
-                    # Untermenü rein
-                    children = self.make_menu(menulevel+1, link)
-                    htmlLink["children"] = children
-
-            result.append(htmlLink)
-
-        return result
+#===============================================================================
+#    def where_filter(self, where_rules):
+#        """
+#        Erweitert das SQL-where Statement um das Rechtemanagement zu
+#        berücksichtigen.
+#        Selbe Funktion ist auch bei sub_menu vorhanden
+#        """
+#        where_rules.append(("showlinks",1))
+#        #~ if not self.session.get("isadmin", False):
+#            #~ where_rules.append(("permitViewPublic",1))
+#
+#        return where_rules
+#
+#    def check_submenu(self, parentID, internal=False):
+#        """
+#        Damit sich das evtl. vorhandene Untermenüpunkt "aufklappt" wird
+#        nachgesehen ob ein Menüpunkt als ParentID die aktuelle SeitenID hat.
+#        """
+#        where_filter = self.where_filter( [("parent",self.current_page_id)] )
+#        # Gibt es Untermenupunkte?
+#        result = self.db.select(
+#                select_items    = ["parent"],
+#                from_table      = "page",
+#                where           = where_filter,
+#                limit           = (0,1)
+#            )
+#
+#        if not result:
+#            # Es gibt keine höhere Ebene (keine Untermenupunkte)
+#            return parentID
+#        else:
+#            # Als startpunkt wird die ParentID eines Untermenupunktes übergeben
+#            return result[0]["parent"]
+#
+#    def create_menudata(self, parentID):
+#        """
+#        Hohlt die relevanten Menüdaten aus der DB in einer Rekursiven-Schleife
+#        """
+#        # Alle Daten der aktuellen Ebene hohlen
+#        parents = self.db.select(
+#                select_items    = ["id","name","shortcut","title","parent"],
+#                from_table      = "page",
+#                where           = self.where_filter( [("parent",parentID)] ),
+#                order           = ("position","ASC")
+#            )
+#        self.menudata.append( parents )
+#
+#        # Hohlt die parentID, um zur nächte Ebene zurück gehen zu können
+#        parent = self.db.select(
+#                select_items    = ["parent"],
+#                from_table      = "page",
+#                where           = [ ("id",parentID) ]
+#            )
+#        if parent:
+#            # Unterste Ebene noch nicht erreicht -> rekursiver Aufruf
+#            self.create_menudata(parent[0]["parent"])
+#
+#    def make_menu(self, menulevel=0, parentname=""):
+#        """
+#        Erstellt das Menü aus self.menudata in einer Rekursiven-Schleife
+#        """
+#        result = []
+#
+#        # Daten der Aktuellen Menüebene
+#        leveldata = self.menudata[menulevel]
+#
+#        if len(self.menudata) > (menulevel+1):
+#            # Es gibt noch eine höhere Menu-Ebene
+#            try:
+#                higher_level_parent = self.menudata[menulevel+1][0]["parent"]
+#            except IndexError:
+#                # Aber nicht, wenn die aktuelle Seite "versteckt" ist
+#                higher_level_parent = False
+#        else:
+#            # Es gibt keine höhere Ebene
+#            higher_level_parent = False
+#
+#        for menuitem in leveldata:
+#            name = menuitem["name"]
+#            title = menuitem["title"]
+#            if title == None:
+#                title = name
+#
+#            link = "%s/%s" % (parentname, menuitem["shortcut"])
+#            linkURL = link#self.URLs.pageLink(link)
+#
+#            htmlLink = {
+#                "level"     : menulevel,
+#                "href"      : linkURL,
+#                "name"      : name,
+#                "title"     : title
+#            }
+#
+#            if menuitem["id"] == self.current_page_id:
+#                # Der aktuelle Menüpunkt ist der "angeklickte"
+#                htmlLink["is_current_page"] = True
+#
+#            if higher_level_parent != False:
+#                # Generell gibt es noch eine höhere Ebene
+#
+#                if menuitem["id"] == higher_level_parent:
+#                    # Es wurde der Menüpunkt erreicht, der das Untermenü
+#                    # "enthält", deswegen kommt ab hier erstmal das
+#                    # Untermenü rein
+#                    children = self.make_menu(menulevel+1, link)
+#                    htmlLink["children"] = children
+#
+#            result.append(htmlLink)
+#
+#        return result
+#===============================================================================
 
 
 
