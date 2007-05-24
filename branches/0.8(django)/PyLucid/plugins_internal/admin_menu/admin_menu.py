6bf95c1d67c52dcfce5a18d3d6bd2cc995867bb3
JensDiemer <JensDiemer@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2007-05-24 06:38:17 +0000

    delete "edit_lock" plugin -> use the Django admin interface to edit styles/templates/internal pages

--- branches/0.8(django)/PyLucid/plugins_internal/admin_menu/admin_menu.py
+++ branches/0.8(django)/PyLucid/plugins_internal/admin_menu/admin_menu.py
@@ -19,20 +19,22 @@ class admin_menu(PyLucidBaseModule):
         """
         Front menu anzeigen
         """
-        current_page = self.context["PAGE"]
-        current_page_id  = current_page.id
+        current_page_id  = self.current_page.id
         edit_link = self.URLs.adminLink("PyLucid/page/%s/" % current_page_id)
 
         context = {
-#            "edit_page_link": self.URLs.commandLink("pageadmin", "edit_page"),
-            "edit_page_link": edit_link,
+            "edit_page_link": self.URLs.commandLink("pageadmin", "edit_page"),
+#            "edit_page_link": edit_link,
             "new_page_link": self.URLs.adminLink("PyLucid/page/add/"),
             "sub_menu_link": self.URLs.commandLink("admin_menu", "sub_menu"),
         }
         self._render_template("top_menu", context)
 
     def sub_menu(self):
-        context = {"commandURLprefix": self.URLs["commandBase"]}
+        context = {
+            "commandURLprefix": self.URLs["commandBase"],
+            "adminURLprefix": self.URLs["adminBase"],
+        }
 #        self.page_msg(context)
 
         self._render_template("sub_menu", context)
