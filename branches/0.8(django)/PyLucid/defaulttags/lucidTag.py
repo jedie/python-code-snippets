b896fff37cdfaf49843f0f69c6f5a3f7c5f91c49
JensDiemer <git@jensdiemer.de>
2007-06-19 10:03:51 +0000

    Bugfix: kwargs for plugins

--- branches/0.8(django)/PyLucid/defaulttags/lucidTag.py
+++ branches/0.8(django)/PyLucid/defaulttags/lucidTag.py
@@ -2,35 +2,55 @@
 """
 the special PyLucid tag who starts a module/plugin with the module manager.
 
-e.g.: {% lucidTag module_name.method_name key1="value1" key2="value2" %}
+e.g.: {% lucidTag plugin_name.method_name key1="value1" key2="value2" %}
 
 registered in: ./PyLucid/defaulttags/__init__.py
 """
 
+import cgi
+
 from PyLucid.system.plugin_manager import run
 from PyLucid.system.response import SimpleStringIO
 
+from django.conf import settings
 from django import template
 
 
+class lucidTagNodeError(template.Node):
+    """
+    display a error messages in the cms page for a wrong lucidTag.
+    """
+    def __init__(self, plugin_name, method_name, msg):
+        self.plugin_name = plugin_name
+        self.method_name = method_name
+        self.msg = msg
+
+    def render(self, context):
+        txt = "[lucidTag %s.%s syntax error: %s]" % (
+            self.plugin_name, self.method_name, self.msg
+        )
+        return txt
+
+
 class lucidTagNode(template.Node):
-    def __init__(self, module_name, method_name, method_kwargs):
-        self.module_name = module_name
+    def __init__(self, plugin_name, method_name, method_kwargs):
+        self.plugin_name = plugin_name
         self.method_name = method_name
         self.method_kwargs = method_kwargs
 
     def __repr__(self):
         return "<lucidTag node ('%s.%s' kwargs:%s)>" % (
-            self.module_name, self.method_name, self.method_kwargs
+            self.plugin_name, self.method_name, self.method_kwargs
         )
 
     def render(self, context):
-#        print "lucidTag.render():", self.module_name, self.method_name
+#        print "lucidTag.render():", self.plugin_name, self.method_name
 
         local_response = SimpleStringIO()
         output = run(
             context, local_response,
-            self.module_name, self.method_name, self.method_kwargs
+            self.plugin_name, self.method_name,
+            method_kwargs = self.method_kwargs
         )
         if output == None:
             content = local_response.getvalue()
@@ -43,7 +63,7 @@ class lucidTagNode(template.Node):
                 " or return a basestring!"
                 " - But %s.%s has returned: %s (%s)"
             ) % (
-                self.module_name, self.method_name,
+                self.plugin_name, self.method_name,
                 repr(output), type(output)
             )
             raise AssertionError(msg)
@@ -56,32 +76,44 @@ class lucidTagNode(template.Node):
 
 def lucidTag(parser, token):
     """
-    {% lucidTag NurEinTag %}
-    {% lucidTag EinParameter par1="value1" %}
-    {% lucidTag ZweiParameter par1="value1" par2="value2" %}
+    Parse the lucidTags.
+
+    syntax e.g.:
+        {% lucidTag PluginName %}
+        {% lucidTag PluginName kwarg1="value1" %}
+        {% lucidTag PluginName kwarg1="value1" kwarg2="value2" %}
     """
 #    print token.contents
 
     # split content:
-    # e.g.: {% lucidTag ZweiParameter par1="value1" par2="value2" %}
-    # module_name = "ZweiParameter"
-    # contents = ['par1="value1"', 'par2="value2"']
-    contents = token.contents.split()[1:]
-    module_name = contents.pop(0)
+    # e.g.: {% lucidTag PluginName kwarg1="value1" kwarg2="value2" %}
+    # plugin_name = "PluginName"
+    # kwargs = ['par1="value1"', 'par2="value2"']
+    kwargs = token.contents.split()[1:]
+    plugin_name = kwargs.pop(0)
 
-    if "." in module_name:
-        module_name, method_name = module_name.split(".")
+    if "." in plugin_name:
+        plugin_name, method_name = plugin_name.split(".")
     else:
         method_name = "lucidTag"
 
-    # split the args into dicts
+    # convert the kwargs list into a dict
     # in..: ['par1="value1"', 'par2="value2"']
     # out.: {'par1': 'value1', 'par2': 'value2'}
     method_kwargs = {}
-    for no, arg in enumerate(contents):
-        key, value = arg.split("=", 1)
+    for no, arg in enumerate(kwargs):
+        try:
+            key, value = arg.split("=", 1)
+            key = key.encode(settings.DEFAULT_CHARSET)
+        except Exception, e:
+            return lucidTagNodeError(
+                plugin_name, method_name,
+                "The key word argument %s is not in the right format. (%s)" % (
+                    cgi.escape(repr(arg)), e
+                )
+            )
         value = value.strip('"')
         method_kwargs[key] = value
 
-    return lucidTagNode(module_name, method_name, method_kwargs)
+    return lucidTagNode(plugin_name, method_name, method_kwargs)
 
