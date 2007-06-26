961fbb471a2bf4d99cf319e9a57db5e61ad487a3
JensDiemer <JensDiemer@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2007-06-26 06:26:03 +0000

    update version string.

--- branches/0.8(django)/PyLucid/__init__.py
+++ branches/0.8(django)/PyLucid/__init__.py
@@ -18,5 +18,5 @@
 """
 
 # PyLucid Version String
-PYLUCID_VERSION = (0, 8, 0,"alpha2")
-PYLUCID_VERSION_STRING = "0.8.0 alpha2"
+PYLUCID_VERSION = (0, 8, 0,"alpha3SVN")
+PYLUCID_VERSION_STRING = "0.8.0 alpha3SVN"
