b4ea13a97b960f0191e9a3f5e527b465362bfbfa
JensDiemer <git@jensdiemer.de>
2007-07-04 10:05:06 +0000

    _install section: init DB data doesn't delete all records! Only delete the preferences...

--- branches/0.8(django)/PyLucid/install/install.py
+++ branches/0.8(django)/PyLucid/install/install.py
@@ -44,107 +44,23 @@ def syncdb(request):
     return Sync_DB(request).start_view()
 
 #______________________________________________________________________________
-class InitDBForm(forms.Form):
-    """
-    django newforms
-    """
-    from glob import glob
-    fnmatch = os.path.join(settings.INSTALL_DATA_DIR, "initial_data.*")
-    fixture_filenames = glob(fnmatch)
-
-    fixture_file = forms.ChoiceField(
-        choices=[(i,i) for i in fixture_filenames],
-        widget=forms.RadioSelect,
-        help_text='Please select a fixture file.'
-    )
-
-dump_template = """
-{% extends "install_base.html" %}
-{% block content %}
-<h1>init DB</h1>
-
-{% if output %}
-    <fieldset><legend>{{ file_name|escape }}</legend>
-        <pre>{{ output|escape }}</pre>
-    </fieldset>
-{% else %}
-    <form method="post">
-        {{ FormData }}
-        <input value="execute" name="execute" type="submit">
-    </form>
-{% endif %}
-{% endblock %}
-"""
-class Init_DB(BaseInstall):
-    def view(self):
-        if "fixture_file" in self.request.POST:
-            # Form has been sended
-            init_values = self.request.POST.copy()
-        else:
-            # Requested the first time -> insert a init codeblock
-            init_values = None
-
-        init_db_form = InitDBForm(init_values)
-        init_db_html = init_db_form.as_p()
-
-        self.context["FormData"] = init_db_html
-
-        output = []
-        if "fixture_file" in self.request.POST and init_db_form.is_valid():
-            fixture_file = init_db_form.clean_data["fixture_file"]
-            format = fixture_file.rsplit(".",1)[1]
-            self.context["file_name"] = fixture_file
-
-            output.append("Read fixture file '%s'..." % fixture_file)
-            try:
-                f = file(fixture_file, "rb")
-        #        import codecs
-        #        f = codecs.open(fixture_filename, "r", "utf-8")
-                fixture = f.read()
-        #        fixture = unicode(fixture, "utf8")
-        #        #fixture = unicode(fixture, errors="replace")
-                f.close()
-            except Exception, e:
-                output.append("Error: %s" % e)
-            else:
-                output.append("OK\n")
-
-                from django.core import serializers
-
-                objects = serializers.deserialize(format, fixture)
-
-                count = 0
-                for object in objects:
-                    try:
-                        object.save()
-                    except Exception, e:
-                        output.append("Error: %s\n" % e)
-                    else:
-                        count += 1
-
-                output.append("%s objects restored\n" % count)
-
-        self.context["output"] = "".join(output)
-        return self._render(dump_template)
-
-def _init_db(request):# deactivated with the unterscore!
-    """
-    2. init DB data
-    """
-    return Init_DB(request).start_view()
-#______________________________________________________________________________
 
 class DB_DumpFakeOptions(object):
     """ Fake optparse options """
     datadir = 'PyLucid/db_dump_datadir'
     verbose = True
     stdout = None
-    remain = None
+    # Remain the records of the tables, default will delete all the records:
+    remain = True
     settings = "PyLucid.settings"
 
 class Init_DB2(BaseInstall):
     def view(self):
         from PyLucid.tools.db_dump import loaddb
+
+        # Delete all prefecrences (only for alpha state!!!)
+        from PyLucid.models import Preference
+        Preference.objects.all().delete()
 
         self._redirect_execute(
             loaddb,
@@ -155,7 +71,8 @@ class Init_DB2(BaseInstall):
 
 def init_db2(request):
     """
-    2. init DB data (using db_dump.py)
+    2. init DB data (using db_dump.py, Note: old preferences lost!)
+    TODO: In the final we should not delete the preferences!
     """
     return Init_DB2(request).start_view()
 
