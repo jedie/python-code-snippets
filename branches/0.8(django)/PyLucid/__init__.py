e4bbaa30b53bf9d0c713473943af2a1a5428d69b
JensDiemer <git@jensdiemer.de>
2007-08-30 08:17:27 +0000

    use django.utils.version.get_svn_revision for the PYLUCID_VERSION_STRING, too. ;)

--- branches/0.8(django)/PyLucid/__init__.py
+++ branches/0.8(django)/PyLucid/__init__.py
@@ -17,6 +17,12 @@
     :license: GNU GPL v3, see LICENSE.txt for more details.
 """
 
+from django.utils.version import get_svn_revision
+
+svn_revision = get_svn_revision("PyLucid")
+
 # PyLucid Version String
-PYLUCID_VERSION = (0, 8, 0,"alpha3SVN")
-PYLUCID_VERSION_STRING = "0.8.0 alpha3SVN"
+PYLUCID_VERSION = (0, 8, 0,"alpha3 " + svn_revision)
+PYLUCID_VERSION_STRING = "0.8.0 alpha3 " + svn_revision
+
+
