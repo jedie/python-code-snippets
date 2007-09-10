b27cf678d723892b2bdcf84f63008853291c3bec
JensDiemer <git@jensdiemer.de>
2007-09-10 19:02:43 +0000

    -add a better exception text for a forgotten settings change (changes in pagestats.py) -hide def create_or_update_superuser from the _install menu (add underscore)

--- branches/0.8(django)/PyLucid/middlewares/pagestats.py
+++ branches/0.8(django)/PyLucid/middlewares/pagestats.py
@@ -23,6 +23,7 @@
 from operator import add
 from time import time
 from django.db import connection
+from django.core.exceptions import ImproperlyConfigured
 
 start_overall = time()
 
@@ -41,8 +42,24 @@ class PageStatsMiddleware(object):
         # get number of db queries before we do anything
         old_queries = len(connection.queries)
 
-        # start the view
-        response = view_func(request, *view_args, **view_kwargs)
+        try:
+            # start the view
+            response = view_func(request, *view_args, **view_kwargs)
+        except AttributeError, e:
+            if str(e)== "'WSGIRequest' object has no attribute 'user'":
+                from django.conf import settings
+                if not 'django.contrib.sessions.middleware.SessionMiddleware' \
+                    in settings.MIDDLEWARE_CLASSES or not \
+                    'django.contrib.auth.middleware.AuthenticationMiddleware' \
+                    in settings.MIDDLEWARE_CLASSES:
+                    msg = (
+                        "You must include the session middleware and the"
+                        " authentication middleware in your settings.py"
+                        " after a syncdb. --- The original error message"
+                        " was: %s"
+                    ) % e
+                    raise ImproperlyConfigured(msg)
+            raise
 
         # Put only the statistic into HTML pages
         if not "html" in response.headers['Content-Type']:
