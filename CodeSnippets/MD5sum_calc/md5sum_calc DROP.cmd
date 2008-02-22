347bc46d6d7c8814b23f984356bc08d2523a4f05
JensDiemer <git@jensdiemer.de>
2008-02-22 09:19:25 +0000

    md5sum_calc -> add small helper file for windows.

---
+++ CodeSnippets/MD5sum_calc/md5sum_calc DROP.cmd
@@ -0,0 +1,8 @@
+@REM
+@REM Helper batch for windows.
+@REM
+@REM Put a link from this file on your desktop.
+@REM Then you can easy drop files on the link.
+cd /d "%~dp0"
+python.exe md5sum_calc.py "%~1"
+@pause
