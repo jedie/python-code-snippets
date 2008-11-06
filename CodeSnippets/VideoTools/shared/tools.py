ee87fb05c74a5660a987e561564e391a20aedcc8
JensDiemer <JensDiemer@8445c942-cd0f-11dd-87b6-9d1a99559f42>
2008-11-06 17:05:06 +0000

    add some missing VideoTools files.

--- CodeSnippets/VideoTools/shared/tools.py
+++ CodeSnippets/VideoTools/shared/tools.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import os
+import os, sys
 
 import Tkinter as tk
-from tkFileDialog import askopenfilename
+from tkFileDialog import askopenfilename, askdirectory
+
+def _ask(method, *args, **kwargs):
+    root = tk.Tk()
+    kwargs["parent"] = root
+    path = method(*args, **kwargs)
+    root.destroy()
+    
+    if path == "": # Nothing selected.
+        sys.exit()
+    
+    return os.path.normpath(path)
+
+def askdirectory2(*args, **kwargs):
+    return _ask(askdirectory, *args, **kwargs)
 
 
 def askopenfilename2(*args, **kwargs):
-    root = tk.Tk()
-    kwargs["parent"] = root
-    path = askopenfilename(*args, **kwargs)
-    root.destroy()
-    
-    if path != "":
-        path = os.path.normpath(path)
-    
-    return path
+    return _ask(askopenfilename, *args, **kwargs)
