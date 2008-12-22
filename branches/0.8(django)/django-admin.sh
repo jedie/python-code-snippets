6ac749a9c63ba89efb9cfee9cf1cf77063a92275
JensDiemer <git@jensdiemer.de>
2008-12-22 09:21:38 +0000

    delete PyLucid, still exist here: http://svn.pylucid.net/pylucid/trunk/ ;)

--- branches/0.8(django)/django-admin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#export DJANGO_SETTINGS_MODULE=PyLucid.settings
-
-# use the local django packages
-export PYTHONPATH=${PWD}
-
-python ./django/bin/django-admin.py --settings=PyLucid.settings $*
