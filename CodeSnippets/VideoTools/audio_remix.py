872d4ab8214a45d11b843f339b9f074b4dabd7f9
JensDiemer <git@jensdiemer.de>
2009-05-29 11:44:28 +0000

    audio remix with map files

--- CodeSnippets/VideoTools/audio_remix.py
+++ CodeSnippets/VideoTools/audio_remix.py
@@ -15,24 +15,57 @@
 """
 import os
 import sys
+import glob
 import subprocess
 import Tkinter as tk
 
 from shared import config, tk_tools, tools
 
-# IMPORTAND: The sort of all channel info must be the same!
-CHANNEL_MAP = (
-    "Front Left",    # 0
-    "Front Center",  # 1
-    "Front Right",   # 2
-    "Surround Left", # 3
-    "Surround Right",# 4
-    "LFE",           # 5
-)
 CHANNELS_BESWEET = ("FL", "C",  "FR", "SL", "SR", "LFE")
 CHANNELS_AFTEN =   ("fl", "fc", "fr", "sl", "sr", "lfe")
 
+CHANNEL_MAP_DIR = "audio_remix_maps"
 OUT_NAME_ADDITION = "_AUDIO_REMIX"
+
+
+def parse_channel_map(txt):
+    """
+    input: 
+        fl  <- 2: SL
+        fc  <- 1: FL
+        fr  <- 2: SR
+        sl  <- 2: SL 
+        sr  <- 2: SR
+        lfe <- 2: LFE
+    returns:
+    [('fl', 2, 'SL'), ('fc', 1, 'FL'), ('fr', 2, 'SR'), ('sl', 2, 'SL'), ('sr', 2, 'SR'), ('lfe', 2, 'LFE')]
+    """
+    channel_map = []
+    for line in txt.splitlines():
+        line = line.strip()
+        if line.startswith("#"):
+            continue
+
+        destination, source = line.split("<-")
+        destination = destination.strip()
+        assert destination in CHANNELS_AFTEN, \
+            "Error in channel map file in line: %r\nDestination channel %r not in %r!" % (
+                line, destination, CHANNELS_AFTEN
+        )
+        source = source.strip()
+        
+        source_no, source_channel = source.split(":")
+        source_no = int(source_no)
+        assert source_no in (1,2), \
+            "Error in channel map file in line: %r\nstream number %r is not 1 or 2!" % (line, source_no)
+        source_channel = source_channel.strip()
+        assert source_channel in CHANNELS_BESWEET, \
+            "Error in channel map file in line: %r\nsource channel %r is not in %r!" % (
+                line, source_channel, CHANNELS_BESWEET
+        )
+        
+        channel_map.append((destination, source_no, source_channel))
+    return channel_map
 
 
 def get_same_prefix(items):
@@ -58,17 +91,32 @@ def select_source_stream(file_path, files):
     return source_stream, second_stream
 
 
-def select_source_channels(log, source_stream):
-    streams = CHANNEL_MAP
+def get_raw_channel_map(log, cfg, script_path):
+    """
+    Select channel map text file via Tk and return the file content.
+    """
+    pathname = os.path.join(script_path, CHANNEL_MAP_DIR, "*.txt")
+    pathname = os.path.abspath(pathname)
+    files = glob.glob(pathname)
+    if not files:
+        raise RuntimeError("No channel maps files found in: %s" % pathname)
+    
     lb = tk_tools.TkListbox(
-        title     = "Please select (%s)" % source_stream,
-        lable     = "Please select channels witch would be used from the source stream file\n%s" % source_stream,
-        items     = streams,
+        title     = "Please select (%s)" % pathname,
+        lable     = "Please select channel map",
+        items     = files,
         selectmode=tk.SINGLE,
-        activated = (0,1,2), # Preselect front channels
+        #activated = (0,1,2), # Preselect front channels
     )
-    log("Selected channels: %r" % lb.selection) # list of selected items.
-    return lb.curselection # tuple containing index of selected items
+    filepath = lb.selection[0] # list of selected items.
+    log("Use channel map file: %r" % filepath)
+    
+    f = file(filepath, "r")
+    raw_channel_map = f.read()
+    f.close()
+    
+    return raw_channel_map
+    
 
 
 def _run(log, cmd):
@@ -87,7 +135,6 @@ def BeSweetDemux(cfg, log, stream_filename, wave_temp_path, out_filename):
     Use eac3to if source stream is DTS (convert it into AC3).
     """
     log("demux %r" % stream_filename)
-    print
     
     if not os.path.isdir(wave_temp_path):
         print "Create out path:", wave_temp_path
@@ -95,40 +142,50 @@ def BeSweetDemux(cfg, log, stream_filename, wave_temp_path, out_filename):
        
     ext = os.path.splitext(stream_filename)[1]
     if ext == ".dts":
-        log("create AC3 file from DTS with eac3to")
         eac3to_output = os.path.join(wave_temp_path, out_filename) + "_temp_640KBits.ac3"
-        cmd = [cfg["eac3to"], stream_filename, eac3to_output, "-640"]
-        _run(log, cmd)
+        if os.path.isfile(eac3to_output):
+            log("Skip creating AC3 file from DTS, because it exist: %s" % eac3to_output)
+        else:
+            log("create AC3 file from DTS with eac3to")
+            cmd = [cfg["eac3to"], stream_filename, eac3to_output, "-640"]
+            _run(log, cmd)
         stream_filename = eac3to_output
     
     outfile = os.path.join(wave_temp_path, out_filename) + "_"
     
+    existing_files = glob.glob(outfile+"*.wav")
+    if existing_files:
+        log("Skip creating wave files, seems to be exist: %r" % existing_files)
+        return
+
     cmd = [cfg["BeSweet"], '-core(', '-input', stream_filename, '-output', outfile, '-6ch', ')']
     _run(log, cmd)
 
 
 def remix(cfg, log, source_stream, second_stream, wave_temp_path, source_out_filename, second_out_filename, \
-                                                                                source_channels_indexes, name_prefix):
+                                                                                    channel_map, name_prefix):
     """
     remix the mono wave files into a new AC3 file.
     """
+    
     out_file = os.path.splitext(source_stream)[0] + OUT_NAME_ADDITION + ".ac3"
     log("Create %s" % out_file)
-    print
     
     cmd = [cfg["aften"]]
     
-    for index, besweet_file_code in enumerate(CHANNELS_BESWEET):
-        if index in source_channels_indexes:
+    for aften_parameter_code, stream_no, besweet_file_code in channel_map:
+        if stream_no == 1:
             filename = source_out_filename
-        else:
+        elif stream_no == 2:
             filename = second_out_filename
+        else:
+            raise RuntimeError("Wrong stream_no!")
             
-        log("use %r from %r" % (besweet_file_code, filename))
+        beseet_filename = filename + "_" + besweet_file_code + ".wav"
+        log("use %r from %s" % (aften_parameter_code, beseet_filename))
         
-        filepath = os.path.join(wave_temp_path, filename + "_" + besweet_file_code + ".wav")
+        filepath = os.path.join(wave_temp_path, beseet_filename)
 
-        aften_parameter_code = CHANNELS_AFTEN[index]
         cmd.append("-ch_%s" % aften_parameter_code)
         cmd.append(filepath)
     
@@ -139,6 +196,9 @@ def remix(cfg, log, source_stream, second_stream, wave_temp_path, source_out_fil
 if __name__ == "__main__":
     cfg = config.VideoToolsConfig()
     cfg.debug()
+    
+    script_path = os.getcwd()
+    print "Script path:", script_path
     
     assert len(sys.argv)==3, "DROP two audio files!"
     files = sys.argv[1:]
@@ -169,29 +229,29 @@ if __name__ == "__main__":
     
     log("source stream: %s" % source_stream)
     log("second stream: %s" % second_stream)
-    print
-    
-    
+    print  
     
     source_out_filename = "source_" + source_stream[prefix_cut_len:]
     second_out_filename = "second_" + second_stream[prefix_cut_len:]
     log("used source out name: %s" % source_out_filename)
     log("used second out name: %s" % second_out_filename)
     print 
-
-    source_channels_indexes = select_source_channels(log, source_stream)
-    log("used channels from source stream: %s" % source_channels_indexes)
-    print
+    
+    raw_channel_map = get_raw_channel_map(log, cfg, script_path)
+    channel_map = parse_channel_map(raw_channel_map)
     
     BeSweetDemux(cfg, log, source_stream, wave_temp_path, source_out_filename)
+    print
     BeSweetDemux(cfg, log, second_stream, wave_temp_path, second_out_filename)
-    
+    print
+       
     remix(
         cfg, log,
         source_stream, second_stream,
         wave_temp_path, source_out_filename, second_out_filename,
-        source_channels_indexes, name_prefix
+        channel_map, name_prefix
     )
+    print
     
     log.close()
     print " -- END -- "
