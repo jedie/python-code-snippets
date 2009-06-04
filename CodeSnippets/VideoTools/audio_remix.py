87f551d0b6523fdde4c0a3576753bca39764c906
JensDiemer <git@jensdiemer.de>
2009-06-04 08:26:36 +0000

    * sox ( http://sox.sourceforge.net/ ) can be used to apply filter on channels. * Try to use NTFS compression.

--- CodeSnippets/VideoTools/audio_remix.py
+++ CodeSnippets/VideoTools/audio_remix.py
@@ -31,9 +31,9 @@ OUT_NAME_ADDITION = "_AUDIO_REMIX"
 def parse_channel_map(txt):
     """
     input: 
-        fl  <- 2: SL
+        fl  <- 2: SL - sox: vol 0.5
         fc  <- 1: FL
-        fr  <- 2: SR
+        fr  <- 2: SR - sox: vol 0.5
         sl  <- 2: SL 
         sr  <- 2: SR
         lfe <- 2: LFE
@@ -43,7 +43,7 @@ def parse_channel_map(txt):
     channel_map = []
     for line in txt.splitlines():
         line = line.strip()
-        if line.startswith("#"):
+        if line.startswith("#") or line=="":
             continue
 
         destination, source = line.split("<-")
@@ -53,6 +53,13 @@ def parse_channel_map(txt):
                 line, destination, CHANNELS_AFTEN
         )
         source = source.strip()
+        
+        temp = source.split("sox:")
+        source = temp[0].strip(" -")
+        if len(temp)==2:
+            sox_cmd = temp[1].strip()
+        else:
+            sox_cmd = None
         
         source_no, source_channel = source.split(":")
         source_no = int(source_no)
@@ -64,7 +71,7 @@ def parse_channel_map(txt):
                 line, source_channel, CHANNELS_BESWEET
         )
         
-        channel_map.append((destination, source_no, source_channel))
+        channel_map.append((destination, source_no, source_channel, sox_cmd))
     return channel_map
 
 
@@ -135,10 +142,6 @@ def BeSweetDemux(cfg, log, stream_filename, wave_temp_path, out_filename):
     Use eac3to if source stream is DTS (convert it into AC3).
     """
     log("demux %r" % stream_filename)
-    
-    if not os.path.isdir(wave_temp_path):
-        print "Create out path:", wave_temp_path
-        os.makedirs(wave_temp_path)
        
     ext = os.path.splitext(stream_filename)[1]
     if ext == ".dts":
@@ -162,7 +165,42 @@ def BeSweetDemux(cfg, log, stream_filename, wave_temp_path, out_filename):
     _run(log, cmd)
 
 
-def remix(cfg, log, source_stream, second_stream, wave_temp_path, source_out_filename, second_out_filename, \
+def apply_sox(cfg, log, source_stream, source_out_filename, channel_map, name_prefix):
+    """
+    Apply sox filter, if needed.
+    """
+    for aften_parameter_code, stream_no, besweet_file_code, sox_cmd in channel_map:
+        if not sox_cmd:
+            continue    
+        
+        if stream_no == 1:
+            filename = source_out_filename
+        elif stream_no == 2:
+            filename = second_out_filename
+        else:
+            raise RuntimeError("Wrong stream_no!")
+        
+        infile = filename + "_" + besweet_file_code + ".wav"
+        outfile = filename + "_" + besweet_file_code + "_sox.wav"
+        
+        if os.path.isfile(outfile):
+            log("Skip sox, output file exist: %s" % outfile)
+            continue
+               
+        cmd = [cfg["sox"],     # sox.exe
+            "--show-progress", # show progress, otherwise we see nothing ;) 
+            "-V3",             # set verbosity
+            infile, outfile    # in/out wave files
+        ]
+        
+        sox_cmds = sox_cmd.split(" ") # FIXME: IMHO this doesn't work in any cases
+        cmd += sox_cmds # add all sox effects, form audio remix map file
+        
+        _run(log, cmd)
+    print
+
+
+def remix_ac3(cfg, log, source_stream, second_stream, wave_temp_path, source_out_filename, second_out_filename, \
                                                                                     channel_map, name_prefix):
     """
     remix the mono wave files into a new AC3 file.
@@ -173,7 +211,7 @@ def remix(cfg, log, source_stream, second_stream, wave_temp_path, source_out_fil
     
     cmd = [cfg["aften"]]
     
-    for aften_parameter_code, stream_no, besweet_file_code in channel_map:
+    for aften_parameter_code, stream_no, besweet_file_code, sox_cmd in channel_map:
         if stream_no == 1:
             filename = source_out_filename
         elif stream_no == 2:
@@ -181,7 +219,11 @@ def remix(cfg, log, source_stream, second_stream, wave_temp_path, source_out_fil
         else:
             raise RuntimeError("Wrong stream_no!")
             
-        beseet_filename = filename + "_" + besweet_file_code + ".wav"
+        if sox_cmd:
+            beseet_filename = filename + "_" + besweet_file_code + "_sox.wav"
+        else:
+            beseet_filename = filename + "_" + besweet_file_code + ".wav"
+            
         log("use %r from %s" % (aften_parameter_code, beseet_filename))
         
         filepath = os.path.join(wave_temp_path, beseet_filename)
@@ -191,6 +233,7 @@ def remix(cfg, log, source_stream, second_stream, wave_temp_path, source_out_fil
     
     cmd.append(out_file)
     _run(log, cmd)
+
 
 
 if __name__ == "__main__":
@@ -237,15 +280,35 @@ if __name__ == "__main__":
     log("used second out name: %s" % second_out_filename)
     print 
     
+    # Get remix info
     raw_channel_map = get_raw_channel_map(log, cfg, script_path)
     channel_map = parse_channel_map(raw_channel_map)
     
+    # Create out dir
+    if not os.path.isdir(wave_temp_path):
+        print "Create out path:", wave_temp_path
+        os.makedirs(wave_temp_path)
+    
+    # Try to use NTFS compression (effective on WAV files ;)
+    cmd=["compact", "/C", wave_temp_path]
+    try:
+        _run(log, cmd)
+    except Exception, err:
+        log("Error using NTFS compression: %s" % err)
+    
+    # source files -> WAV files
     BeSweetDemux(cfg, log, source_stream, wave_temp_path, source_out_filename)
     print
     BeSweetDemux(cfg, log, second_stream, wave_temp_path, second_out_filename)
     print
+    
+    # Apply sox filter, if needed
+    os.chdir(wave_temp_path)
+    apply_sox(cfg, log, source_stream, source_out_filename, channel_map, name_prefix)
+    os.chdir(base_path)
        
-    remix(
+    # Remix into final AC3 file
+    remix_ac3(
         cfg, log,
         source_stream, second_stream,
         wave_temp_path, source_out_filename, second_out_filename,
