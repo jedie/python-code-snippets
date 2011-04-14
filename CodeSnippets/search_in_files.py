9d22d6656b228fc3a1dbf1e7788682a0fe74c53d
JensDiemer <git@jensdiemer.de>
2011-04-14 12:02:34 +0200

    use followlinks=True in os.walk()

--- CodeSnippets/search_in_files.py
+++ CodeSnippets/search_in_files.py
@@ -5,10 +5,10 @@
 Sucht rekursiv in Dateiinhalten und listet die Fundstellen auf.
 """
 
-__author__  = "Jens Diemer"
+__author__ = "Jens Diemer"
 __license__ = """GNU General Public License v2 or above -
  http://www.opensource.org/licenses/gpl-license.php"""
-__url__     = "http://www.jensdiemer.de"
+__url__ = "http://www.jensdiemer.de"
 __version__ = "0.1"
 
 
@@ -18,25 +18,25 @@ import os, time, fnmatch
 
 class search:
     def __init__(self, path, search_string, file_filter):
-        self.search_path    = path
-        self.search_string  = search_string
-        self.file_filter     = file_filter
+        self.search_path = path
+        self.search_string = search_string
+        self.file_filter = file_filter
 
         print "Search '%s' in [%s]..." % (
             self.search_string, self.search_path
        )
-        print "_"*80
+        print "_" * 80
 
         time_begin = time.time()
         file_count = self.walk()
-        print "_"*80
+        print "_" * 80
         print "%s files searched in %0.2fsec." % (
-            file_count, (time.time()-time_begin)
+            file_count, (time.time() - time_begin)
        )
 
     def walk(self):
         file_count = 0
-        for root, dirlist, filelist in os.walk(self.search_path):
+        for root, dirlist, filelist in os.walk(self.search_path, followlinks=True):
             for filename in filelist:
                 for file_filter in self.file_filter:
                     if fnmatch.fnmatch(filename, file_filter):
@@ -61,17 +61,17 @@ class search:
             except ValueError:
                 break
 
-            content_window = content[ pos-content_extract : pos+content_extract ]
+            content_window = content[ pos - content_extract : pos + content_extract ]
             print ">>>", content_window.encode("String_Escape")
             current_pos += pos + search_string_len
         print
 
 
 if __name__ == "__main__":
-    search_path     = r"c:\texte"
-    file_filter     = ("*.py",) # fnmatch-Filter
-    search_string   = "history"
-    content_extract = 35 # Größe des Ausschnittes der angezeigt wird
-    max_cutouts     = 20 # Max. Anzahl an Treffer, die Angezeigt werden sollen
+    search_path = r"c:\texte"
+    file_filter = ("*.py",) # fnmatch-Filter
+    search_string = "history"
+    content_extract = 35 # Grï¿½ï¿½e des Ausschnittes der angezeigt wird
+    max_cutouts = 20 # Max. Anzahl an Treffer, die Angezeigt werden sollen
 
     search(search_path, search_string, file_filter)
