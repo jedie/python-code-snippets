164e852db9d567ff8d8d70cecfc32e6efcee2d7e
JensDiemer <git@jensdiemer.de>
2015-08-10 09:54:22 +0200

    try to pull current branch

--- CodeSnippets/update_virtualenv_git_repos.py
+++ CodeSnippets/update_virtualenv_git_repos.py
@@ -10,6 +10,7 @@
 import subprocess
 import os
 import sys
+import re
 
 try:
     import click
@@ -17,6 +18,24 @@ except ImportError as err:
     print("Error: %s" % err)
     print("Please install click!")
     sys.exit(-1)
+
+
+def verbose_check_output(*args, **kwargs):
+    """ 'verbose' version of subprocess.check_output() """
+    verbose=kwargs.pop("verbose", False)
+    if verbose:
+        click.echo(click.style("\tcall: %s" % " ".join(args), fg="blue"))
+    kwargs["universal_newlines"]=True
+    kwargs["stderr"]=subprocess.STDOUT
+    try:
+        output = subprocess.check_output(args, **kwargs)
+    except subprocess.CalledProcessError as err:
+        print("\n***ERROR:")
+        print(err.output)
+        raise
+    if verbose:
+        click.echo(click.style("output:\n%s" % output, fg="blue"))
+    return output
 
 
 def verbose_call(*args, **kwargs):
@@ -29,8 +48,26 @@ def verbose_call(*args, **kwargs):
     click.echo(click.style("(return code: %s)" % returncode, fg=color))
     return returncode
 
-
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+REXP = re.compile(r"^\*\s*(.*)$", re.MULTILINE)
+def get_git_branch(path, verbose=False):
+    output = verbose_check_output(
+        "git", "branch", "--no-color",
+        cwd=path,
+        verbose=verbose
+    )
+    branch = REXP.findall(output)
+    if len(branch)!=1:
+        print("Error finding git branch from output:")
+        print(output)
+        print("-"*79)
+        return None
+    return branch[0]
+
+
+
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument("path", type=click.Path(
@@ -62,9 +99,27 @@ def update_env(path):
         click.echo(click.style("Update %r:" % path, bold=True))
 
         verbose_call("git", "fetch", "--all", cwd=abs_path)
-        verbose_call("git", "pull", cwd=abs_path)
+
+        pull_args = ["git", "pull"]
+        branch = get_git_branch(
+            abs_path,
+            # verbose=True
+        )
+        if branch:
+            # FIXME: How to removed hardcoded 'origin'?
+            pull_args += ["origin", branch]
+
+        verbose_call(*pull_args, cwd=abs_path)
 
 
 if __name__ == "__main__":
     update_env()
     click.echo("\nBye\n")
+
+    # print(
+    #     get_git_branch(
+    #         os.path.expanduser("~/PyLucid_env/src/pylucid"),
+    #         verbose=True
+    #     )
+    # )
+
