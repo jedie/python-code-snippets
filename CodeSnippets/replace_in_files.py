96860f3842bd637d4258d84e2a686aec94b9705b
JensDiemer <git@jensdiemer.de>
2019-01-10 10:14:22 +0100

    code formatting

--- CodeSnippets/replace_in_files.py
+++ CodeSnippets/replace_in_files.py
@@ -1,69 +1,64 @@
-#!/usr/bin/python -O
-# coding: UTF-8
+#!/usr/bin/env python3
 
-from __future__ import print_function, unicode_literals
 
 """
--replace string in files (recursive)
--display the difference.
+    -replace string in files (recursive)
+    -display the difference.
 
-v0.3
- - update for Python v3
-v0.2
- - search_string can be a re.compile() object -> use re.sub for replacing
+    v0.3
+     - update for Python v3
+    v0.2
+     - search_string can be a re.compile() object -> use re.sub for replacing
 
-v0.1
- - initial version
+    v0.1
+     - initial version
 
 
-    Useable by a small "client" script, e.g.:
+        Useable by a small "client" script, e.g.:
 
--------------------------------------------------------------------------------
-#!/usr/bin/python -O
-# coding: UTF-8
+    -------------------------------------------------------------------------------
+    #!/usr/bin/env python3
 
-from __future__ import print_function, unicode_literals
+    import sys, re
 
-import sys, re
+    #sys.path.insert(0,"/path/to/git/repro/") # Please change path
 
-#sys.path.insert(0,"/path/to/git/repro/") # Please change path
+    from replace_in_files import SearchAndReplace
 
-from replace_in_files import SearchAndReplace
+    SearchAndReplace(
+        search_path = "/to/the/files/",
 
-SearchAndReplace(
-    search_path = "/to/the/files/",
+        # e.g.: simple string replace:
+        search_string  = b'the old string',
+        replace_string = b'the new string',
 
-    # e.g.: simple string replace:
-    search_string  = b'the old string',
-    replace_string = b'the new string',
+        # e.g.: Regular expression replacing (used re.sub)
+        #search_string  = re.compile('{% url (.*?) %}'),
+        #replace_string = "{% url '\g<1>' %}",
 
-    # e.g.: Regular expression replacing (used re.sub)
-    #search_string  = re.compile('{% url (.*?) %}'),
-    #replace_string = "{% url '\g<1>' %}",
+        search_only = True, # Display only the difference
+        #search_only = False, # write the new content
 
-    search_only = True, # Display only the difference
-    #search_only = False, # write the new content
+        file_filter=("*.py",), # fnmatch-Filter
+    )
+    -------------------------------------------------------------------------------
 
-    file_filter=("*.py",), # fnmatch-Filter
-)
--------------------------------------------------------------------------------
-
-:copyleft: 2009-2016 by Jens Diemer
+    :copyleft: 2009-2019 by Jens Diemer
 """
 
-__author__ = "Jens Diemer"
-__license__ = """GNU General Public License v3 or above -
- http://www.opensource.org/licenses/gpl-license.php"""
-__url__ = "http://www.jensdiemer.de"
-__version__ = "0.4"
-
+import difflib
+import fnmatch
 import io
 import os
 import re
 import sys
 import time
-import fnmatch
-import difflib
+
+__author__ = "Jens Diemer"
+__license__ = """GNU General Public License v3 or above -
+ http://www.opensource.org/licenses/gpl-license.php"""
+__url__ = "http://www.jensdiemer.de"
+__version__ = "0.5"
 
 
 # FIXME: see http://stackoverflow.com/questions/4730121/cant-get-an-objects-class-name-in-python
@@ -71,8 +66,7 @@ RE_TYPE = type(re.compile(""))
 
 
 class SearchAndReplace(object):
-    def __init__(self, search_path, search_string, replace_string,
-                 search_only=True, file_filter=("*.*",)):
+    def __init__(self, search_path, search_string, replace_string, search_only=True, file_filter=("*.*",)):
         self.search_path = search_path
         self.search_string = search_string
         self.replace_string = replace_string
@@ -84,9 +78,7 @@ class SearchAndReplace(object):
         # FIXME: see http://stackoverflow.com/questions/4730121/cant-get-an-objects-class-name-in-python
         self.is_re = isinstance(self.search_string, RE_TYPE)
 
-        print("Search '%s' in [%s]..." % (
-            self.search_string, self.search_path
-        ))
+        print("Search '%s' in [%s]..." % (self.search_string, self.search_path))
         print("_" * 80)
 
         time_begin = time.time()
@@ -94,9 +86,7 @@ class SearchAndReplace(object):
         file_count = self.walk()
 
         print("_" * 80)
-        print("%s files searched in %0.2fsec." % (
-            file_count, (time.time() - time_begin)
-        ))
+        print("%s files searched in %0.2fsec." % (file_count, (time.time() - time_begin)))
 
     def walk(self):
         file_count = 0
@@ -136,14 +126,12 @@ class SearchAndReplace(object):
             if new_content == old_content:
                 return old_content
         else:
-            new_content = old_content.replace(
-                self.search_string, self.replace_string
-            )
+            new_content = old_content.replace(self.search_string, self.replace_string)
 
         if self.search_only != False:
             return new_content
 
-        print("Write new content into %s..." % filepath, end=' ')
+        print("Write new content into %s..." % filepath, end=" ")
         try:
             with io.open(filepath, "wb") as f:
                 f.write(new_content)
@@ -208,17 +196,13 @@ class SearchAndReplace(object):
 if __name__ == "__main__":
     SearchAndReplace(
         search_path=".",
-
         # e.g.: simple string replace:
-        search_string=b'the old string',
-        replace_string=b'the new string',
-
+        search_string=b"the old string",
+        replace_string=b"the new string",
         # e.g.: Regular expression replacing (used re.sub)
-        #search_string   = re.compile('{% url (.*?) %}'),
-        #replace_string  = "{% url '\g<1>' %}",
-
-        search_only=True, # Display only the difference
-#        search_only     = False, # write the new content
-
-        file_filter=("*.py",), # fnmatch-Filter
+        # search_string   = re.compile('{% url (.*?) %}'),
+        # replace_string  = "{% url '\g<1>' %}",
+        search_only=True,  # Display only the difference
+        #        search_only     = False, # write the new content
+        file_filter=("*.py",),  # fnmatch-Filter
     )
