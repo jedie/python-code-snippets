4f2902417ed7ea2d54eb01f469b8d1a855d6e43c
JensDiemer <git@jensdiemer.de>
2019-01-10 10:14:09 +0100

    python3 updates

--- CodeSnippets/search_in_files.py
+++ CodeSnippets/search_in_files.py
@@ -1,40 +1,36 @@
-#!/usr/bin/python -O
-# -*- coding: UTF-8 -*-
+#!/usr/bin/env python3
 
 """
-Sucht rekursiv in Dateiinhalten und listet die Fundstellen auf.
+    Sucht rekursiv in Dateiinhalten und listet die Fundstellen auf.
 """
 
 __author__ = "Jens Diemer"
 __license__ = """GNU General Public License v2 or above -
  http://www.opensource.org/licenses/gpl-license.php"""
 __url__ = "http://www.jensdiemer.de"
-__version__ = "0.1"
+__version__ = "0.2"
 
 
-
-import os, time, fnmatch
+import fnmatch
+import os
+import time
 
 
 class Search:
-    def __init__(self, path, search_string, file_filter, max_cutouts=20, content_extract = 35):
+    def __init__(self, path, search_string, file_filter, max_cutouts=20, content_extract=35):
         self.search_path = path
         self.search_string = search_string
         self.file_filter = file_filter
         self.max_cutouts = max_cutouts
         self.content_extract = content_extract
 
-        print "Search '%s' in [%s]..." % (
-            self.search_string, self.search_path
-       )
-        print "_" * 80
+        print("Search '%s' in [%s]..." % (self.search_string, self.search_path))
+        print("_" * 80)
 
         time_begin = time.time()
         file_count = self.walk()
-        print "_" * 80
-        print "%s files searched in %0.2fsec." % (
-            file_count, (time.time() - time_begin)
-       )
+        print("_" * 80)
+        print("%s files searched in %0.2fsec." % (file_count, (time.time() - time_begin)))
 
     def walk(self):
         file_count = 0
@@ -51,29 +47,29 @@ class Search:
         content = f.read()
         f.close()
         if self.search_string in content:
-            print filepath
+            print(filepath)
             self.cutout_content(content)
 
     def cutout_content(self, content):
         current_pos = 0
         search_string_len = len(self.search_string)
-        for i in xrange(self.max_cutouts):
+        for i in range(self.max_cutouts):
             try:
                 pos = content.index(self.search_string, current_pos)
             except ValueError:
                 break
 
-            content_window = content[ pos - self.content_extract : pos + self.content_extract ]
-            print ">>>", content_window.encode("String_Escape")
+            content_window = content[pos - self.content_extract : pos + self.content_extract]
+            print(">>>", content_window.encode("String_Escape"))
             current_pos += pos + search_string_len
-        print
+        print()
 
 
 if __name__ == "__main__":
     search_path = r"c:\texte"
-    file_filter = ("*.py",) # fnmatch-Filter
+    file_filter = ("*.py",)  # fnmatch-Filter
     search_string = "history"
-    content_extract = 35 # Gr��e des Ausschnittes der angezeigt wird
-    max_cutouts = 20 # Max. Anzahl an Treffer, die Angezeigt werden sollen
+    content_extract = 35  # Größe des Ausschnittes der angezeigt wird
+    max_cutouts = 20  # Max. Anzahl an Treffer, die Angezeigt werden sollen
 
     Search(search_path, search_string, file_filter, max_cutouts, content_extract)
