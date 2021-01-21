4f3de0dbbe7e1486eba8ded91a1d60888fb7051c
JensDiemer <git@jensdiemer.de>
2021-01-21 19:36:49 +0100

    quick&dirty update to python3

--- CodeSnippets/bitcoin/wallet_info.py
+++ CodeSnippets/bitcoin/wallet_info.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
+#!/usr/bin/env python3
 
 """
     wallet info
@@ -26,14 +25,14 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-import os
-import sys
-import bsddb
 import datetime
-import tempfile
-import struct
 import mmap
+import os
+import struct
+import sys
+import tempfile
 
+import bsddb3 # https://pypi.org/project/bsddb3/
 
 LINK_URLS = (
     ("blockchain.info", "http://blockchain.info/address/%s"),
@@ -52,7 +51,8 @@ OWN_FILENAME = os.path.basename(__file__)
 class SerializationError(Exception):
     """ Thrown when there's a problem deserializing or serializing """
 
-class BCDataStream(object):
+
+class BCDataStream:
     def __init__(self):
         self.input = None
         self.read_cursor = 0
@@ -61,17 +61,19 @@ class BCDataStream(object):
         self.input = None
         self.read_cursor = 0
 
-    def write(self, bytes): # Initialize with string of bytes
+    def write(self, bytes):  # Initialize with string of bytes
         if self.input is None:
             self.input = bytes
         else:
             self.input += bytes
 
-    def map_file(self, file, start): # Initialize with bytes from file
+    def map_file(self, file, start):  # Initialize with bytes from file
         self.input = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
         self.read_cursor = start
+
     def seek_file(self, position):
         self.read_cursor = position
+
     def close_file(self):
         self.input.close()
 
@@ -91,7 +93,8 @@ class BCDataStream(object):
         except IndexError:
             raise SerializationError("attempt to read past end of buffer")
 
-        return self.read_bytes(length)
+        b = self.read_bytes(length)
+        return b.decode('utf-8')
 
     def write_string(self, string):
         # Length-encoded as with read-string
@@ -105,8 +108,6 @@ class BCDataStream(object):
             return result
         except IndexError:
             raise SerializationError("attempt to read past end of buffer")
-
-        return ''
 
     def read_boolean(self): return self.read_bytes(1)[0] != chr(0)
     def read_int16(self): return self._read_num('<h')
@@ -125,7 +126,7 @@ class BCDataStream(object):
     def write_uint64(self, val): return self._write_num('<Q', val)
 
     def read_compact_size(self):
-        size = ord(self.input[self.read_cursor])
+        size = self.input[self.read_cursor]
         self.read_cursor += 1
         if size == 253:
             size = self._read_num('<H')
@@ -160,32 +161,34 @@ class BCDataStream(object):
         self.write(s)
 
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 
 def get_temp_copy(wallet_filepath):
-    print "Make temp file from %r..." % wallet_filepath
+    print(f"Make temp file from {wallet_filepath!r}...")
     with open(wallet_filepath, "rb") as f:
         temp_wallet = tempfile.NamedTemporaryFile(prefix=OWN_FILENAME)
         temp_wallet.write(f.read())
-    print "temp tile %r created" % temp_wallet.name
+    print(f"temp tile {temp_wallet.name!r} created")
     return temp_wallet
 
 
 def get_wallet_db(temp_wallet):
     dir, filename = os.path.split(temp_wallet.name)
 
-    print "open bsddb at directory %r..." % dir
-    db_env = bsddb.db.DBEnv()
-    db_env.open(dir,
-        # bsddb.db.DB_INIT_LOCK
-        (bsddb.db.DB_CREATE | bsddb.db.DB_INIT_LOCK | bsddb.db.DB_INIT_LOG | bsddb.db.DB_INIT_MPOOL | bsddb.db.DB_INIT_TXN | bsddb.db.DB_THREAD | bsddb.db.DB_RECOVER)
+    print(f"open bsddb at directory {dir!r}...")
+    db_env = bsddb3.db.DBEnv()
+    db_env.open(
+        dir,
+        # bsddb3.db.DB_INIT_LOCK
+        (bsddb3.db.DB_CREATE | bsddb3.db.DB_INIT_LOCK | bsddb3.db.DB_INIT_LOG |
+         bsddb3.db.DB_INIT_MPOOL | bsddb3.db.DB_INIT_TXN | bsddb3.db.DB_THREAD | bsddb3.db.DB_RECOVER)
     )
 
-    db = bsddb.db.DB(db_env)
+    db = bsddb3.db.DB(db_env)
 
-    print "open file %r..." % filename
-    db.open(filename, "main", bsddb.db.DB_BTREE, bsddb.db.DB_RDONLY)
+    print(f"open file {filename!r}...")
+    db.open(filename, "main", bsddb3.db.DB_BTREE, bsddb3.db.DB_RDONLY)
     return db
 
 
@@ -195,20 +198,21 @@ def extract_addresses(db):
 
     addr_info = []
 
-    for key, value in db.items():
+    for key, value in list(db.items()):
         kds.clear()
         kds.write(key)
         item_type = kds.read_string()
 
-#         print item_type, repr(key), repr(value)
+        # print(item_type, repr(key), repr(value))
 
         vds.clear()
         vds.write(value)
 
         if item_type == "name":
+            print(item_type, repr(key), repr(value))
             hash = kds.read_string()
             name = vds.read_string()
-            print hash, name
+            print(hash, name)
             addr_info.append(
                 (hash, name)
             )
@@ -221,9 +225,9 @@ def get_addr_info(wallet_filepath):
         try:
             addr_info = extract_addresses(db)
         finally:
-            print
-            print "Close db %r" % temp_wallet.name
-            print
+            print()
+            print(f"Close db {temp_wallet.name!r}")
+            print()
             db.close()
 
 #         print addr_info
@@ -233,33 +237,32 @@ def get_addr_info(wallet_filepath):
 def create_html_info(wallet_filepath):
     addr_info = get_addr_info(wallet_filepath)
 
-    html_filepath = "%s.html" % os.path.splitext(wallet_filepath)[0]
-    print
-    print "Create %r..." % html_filepath
+    html_filepath = f"{os.path.splitext(wallet_filepath)[0]}.html"
+    print()
+    print(f"Create {html_filepath!r}...")
 
     with open(html_filepath, "w") as f:
         f.write("<!DOCTYPE html><html><head>")
-        f.write("<title>%s</title>" % os.path.basename(wallet_filepath))
+        f.write(f"<title>{os.path.basename(wallet_filepath)}</title>")
         f.write("</head><body>")
-        f.write("<h1>%s</h1>" % wallet_filepath)
+        f.write(f"<h1>{wallet_filepath}</h1>")
         f.write("<table>")
 
         for hash, name in addr_info:
             f.write("<tr>")
-            f.write("<td>%s</td>" % hash)
+            f.write(f"<td>{hash}</td>")
 
             urls = []
             for txt, url in LINK_URLS:
                 url = url % hash
-                urls.append('<a href="%s">%s</a>' % (url, txt))
+                urls.append(f'<a href="{url}">{txt}</a>')
 
-            f.write("<td>%s</td>" % "&nbsp;|&nbsp;".join(urls))
-            f.write("<td>%s</td>" % name)
+            f.write(f"<td>{'&nbsp;|&nbsp;'.join(urls)}</td>")
+            f.write(f"<td>{name}</td>")
             f.write("</tr>")
         f.write("</table>")
-        f.write("<address>Created with <stroing>%s</strong> at %s</address>" % (
-            OWN_FILENAME, datetime.datetime.now().strftime("%c")
-        ))
+        f.write(
+            f"<address>Created with <stroing>{OWN_FILENAME}</strong> at {datetime.datetime.now().strftime('%c')}</address>")
         f.write("</body></html>")
 
 
@@ -268,17 +271,17 @@ if __name__ == "__main__":
     for arg in sys.argv[1:]:
         if os.path.isfile(arg):
             count += 1
-            print "_"*79
-            print arg
-            print
+            print("_" * 79)
+            print(arg)
+            print()
             create_html_info(arg)
-            print "\n"*2
+            print("\n" * 2)
         else:
-            print "Ignore %r (not a file)"
+            print("Ignore %r (not a file)")
 
     if count == 0:
-        print "Add 'wallet.dat' files as argument!"
+        print("Add 'wallet.dat' files as argument!")
     else:
-        print "Process %i wallet files." % count
+        print("Process %i wallet files." % count)
 
-    print " -- END -- "
+    print(" -- END -- ")
